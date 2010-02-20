5e913a61815b4edca25214f43e9b12d907d9b3be
Brian M <michab3@rpi.edu>
2010-02-20 18:26:22 -0500

    Adding some scoping so you can do cool things like Feed.find(1).approved_contents.active to get all the approved active content

--- app/models/content.rb
+++ app/models/content.rb
@@ -12,6 +12,11 @@ class Content < ActiveRecord::Base
   #validates :user, :associated => true
   #validates :type, :associated => true
 
+  #Easily query for active, expired, or future content
+  scope :expired, where("end_time < :now", {:now => Time.now})
+  scope :future, where("start_time > :now", {:now => Time.now})
+  scope :active, where("(start_time IS NULL OR start_time < :now) AND (end_time IS NULL OR end_time > :now)", {:now => Time.now})
+
   #Determine if content is active based on its start and end times.
   def is_active?
     (start_time.nil? || start_time < Time.now) && (end_time.nil? || end_time > Time.now)
--- app/models/feed.rb
+++ app/models/feed.rb
@@ -3,6 +3,11 @@ class Feed < ActiveRecord::Base
   has_many :submissions
   has_many :contents, :through => :submissions
 
+  #Scoped relations for content approval states
+  has_many :approved_contents, :through => :submissions, :source => :content, :conditions => {"submissions.moderation_flag" => true}
+  has_many :pending_contents, :through => :submissions, :source => :content, :conditions => "submissions.moderation_flag IS NULL"
+  has_many :denied_contents, :through => :submissions, :source => :content, :conditions => {"submissions.moderation_flag" => false}
+
   #Validations
   validates :name, :presence => true
 end
