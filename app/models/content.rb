26ac35efcc580227d2191106e8729c51c3a6ad83
Brian M <michab3@rpi.edu>
2010-02-19 22:50:17 -0500

    A few content validations, small is_active? method.

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,4 +1,18 @@
 class Content < ActiveRecord::Base
   belongs_to :user
   belongs_to :type
+
+  #Validations
+  validates :name, :presence => true
+  validates :mime_type, :presence => true
+
+  #Enable more validations when the models are flushed out.
+  #validates :user, :associated => true
+  #validates :type, :associated => true
+
+  #Determine if content is active based on its start and end times.
+  def is_active?
+    (start_time.nil? || start_time < Time.now) && (end_time.nil? || end_time > Time.now)
+  end
+
 end
