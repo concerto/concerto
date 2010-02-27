2b85b9b6f8cf10caadfe28606c252d2ee2979e56
Brian M <michab3@rpi.edu>
2010-02-26 23:43:18 -0500

    Submission unit tests, found bug in feed fixture (duplicate name).

--- test/unit/submission_test.rb
+++ test/unit/submission_test.rb
@@ -1,8 +1,43 @@
 require 'test_helper'
 
 class SubmissionTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  #Test for required associations
+  test "submission requires feed" do
+    blank = Submission.new()
+    assert !blank.valid?
+    
+    s = Submission.new({:content => contents(:one)})
+    assert !s.valid?, "Submission doesn't have feed"
+    s.feed = feeds(:one)
+    assert s.valid?, "Submission has feed"
+  end
+  test "submission requires content" do
+    s = Submission.new({:feed => feeds(:one)})
+    assert !s.valid?, "Submission doesn't have content"
+    s.content_id = contents(:one).id
+    assert s.valid?, "Submission has content"
+  end
+  
+  #Test scoping
+  test "approved content" do 
+    s = submissions(:one_one)
+    approved = Submission.approved.all
+    assert s.moderation_flag, "Submission approved"
+    assert_equal approved.length, 1, "Only 1 approved"
+    assert_equal approved.first, s, "Submission matches approved"
+  end
+  test "denied content" do 
+    s = submissions(:one_two)
+    denied = Submission.denied.all
+    assert !s.moderation_flag, "Submission denied"
+    assert_equal denied.length, 1, "Only 1 denied"
+    assert_equal denied.first, s, "Submission matches denied"
+  end
+  test "pending content" do 
+    s = submissions(:old_one)
+    pending = Submission.pending.all
+    assert s.moderation_flag.nil?, "Submission pending"
+    assert_equal pending.length, 1, "Only 1 pending"
+    assert_equal pending.first, s, "Submission matches pending"
   end
 end
