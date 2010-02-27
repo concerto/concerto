bce86d14a7a5da489ce5382c83ec3b54d36cebcd
Brian M <michab3@rpi.edu>
2010-02-26 23:08:54 -0500

    The type tests are the same as the feed tests ATM.

--- test/unit/type_test.rb
+++ test/unit/type_test.rb
@@ -1,8 +1,23 @@
 require 'test_helper'
 
 class TypeTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  #Test the fields that should be required
+  test "name cannot be blank" do
+    t = types(:text)
+    type = Type.new(t.attributes)
+    type.name = ""
+    assert !type.valid?, "Type name is blank"
+    type.name = "Blah"
+    assert type.valid?, "Type name has entry"
+  end
+  
+  #Test for duplicate names
+  test "name cannot be duplicated" do
+    t = types(:text)
+    type = Type.new({:name => t.name})
+    assert_equal t.name, type.name, "Names are set equal"
+    assert !type.valid?, "Names can't be equal"
+    type.name = "Fooasdasdasda"
+    assert type.valid?, "Unique name is OK"
   end
 end
