c5cf9f05dc9a2c023079b15d1401a5cb3c04f6dd
Brian M <michab3@rpi.edu>
2010-02-26 23:05:40 -0500

    Basic unit tests for feeds (validations)

--- test/unit/feed_test.rb
+++ test/unit/feed_test.rb
@@ -1,8 +1,23 @@
 require 'test_helper'
 
 class FeedTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  #Test the fields that should be required
+  test "name cannot be blank" do
+    f = feeds(:one)
+    feed = Feed.new(f.attributes)
+    feed.name = ""
+    assert !feed.valid?, "Feed name is blank"
+    feed.name = "Blah"
+    assert feed.valid?, "Feed name has entry"
+  end
+  
+  #Test for duplicate names
+  test "name cannot be duplicated" do
+    f = feeds(:one)
+    feed = Feed.new({:name => f.name})
+    assert_equal f.name, feed.name, "Names are set equal"
+    assert !feed.valid?, "Names can't be equal"
+    feed.name = "Fooasdasdasda"
+    assert feed.valid?, "Unique name is OK"
   end
 end
