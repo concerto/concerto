fdcff3c1d877d1e71e62aac819447e63b88298d9
Brian M <michab3@rpi.edu>
2010-02-26 23:44:15 -0500

    The submission test found two bugs, who knew these things were useful.

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -4,10 +4,11 @@ class Submission < ActiveRecord::Base
   belongs_to :user
   
   #Validations
-  validates_associated :feed, :content
+  validates :feed, :presence => true, :associated => true
+  validates :content, :presence => true, :associated => true
 
   #Scoping shortcuts for active/denied/pending
   scope :approved, where(:moderation_flag => true)
-  scope :denied, where(:moderation_flag => true)
+  scope :denied, where(:moderation_flag => false)
   scope :pending, where("moderation_flag IS NULL") 
 end
