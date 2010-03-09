e1eb8779717558b407b0d70f6ab1ad7523c1fdd5
Brian M <michab3@rpi.edu>
2010-03-09 13:59:15 -0500

    Updating unit test to reflect required owners.  Polymorphic join makes everything messy!

--- app/models/screen.rb
+++ app/models/screen.rb
@@ -4,6 +4,9 @@ class Screen < ActiveRecord::Base
 
   #Validations
   validates :name, :presence => true
+  #These two validations are used to solve problems with the polymorphic 
+  #presence and associated tests.
+  validates :owner_id, :presence => true
   validates_inclusion_of :owner_type, :in => %w( User Group )
   #The below validation fails loudly if the owner_type isn't a valid class
   #For now, the check will be string based, it should probably be moved to
--- test/unit/screen_test.rb
+++ test/unit/screen_test.rb
@@ -1,8 +1,37 @@
 require 'test_helper'
 
 class ScreenTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  #Test the fields that should be required
+  test "name cannot be blank" do
+    s = screens(:one)
+    screen = Screen.new(s.attributes)
+    screen.owner = users(:katie)
+    screen.name = ""
+    assert !screen.valid?, "Screen name is blank"
+    screen.name = "Blah"
+    assert screen.valid?, "Screen name has entry"
+  end
+  test "owner must be group or user" do
+    s = screens(:one)
+    s.owner = users(:katie)
+    assert s.valid?, "Screen owned by user"
+    s.owner_type = "Pants"
+    assert !s.valid?, "Screen cannot be owner by pants"
+    s.owner_type = "User"
+    assert s.valid?, "Screen can be owner by user"
+    s.owner = groups(:wtg)
+    assert s.valid?, "Screen can be owner by group"
+    s.owner = nil
+    s.owner_type = "Group"
+    s.owner_id = ""
+    assert !s.valid?, "Screen owner must be set"
+    s.owner = nil
+    s.owner_type = ""
+    s.owner_id = users(:kristen).id
+    assert !s.valid?, "Screen owner type must be set"
+    s.owner = nil
+    s.owner_type = "User"
+    s.owner_id = 0
+    assert !s.valid?, "Screen owner must be valid"
   end
 end
