8dd4463dfbfb5abff141ae40e9f83e59ef9b500e
Brian M <michab3@rpi.edu>
2010-06-06 17:42:27 -0400

    Fixing deprecated session store config.  [#95 state:resolved responsible:"Brian Michalski"]

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random, 
 # no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = '55fed69ff18aa21f4ab5d3482e368c9733595fb4428e116f31ab45f4eab8e712ea87ac4daeec032d2a6c2301a8ca376f070927e65e2c609890c2e0a314948d3a'
+Rails.application.config.secret_token = '55fed69ff18aa21f4ab5d3482e368c9733595fb4428e116f31ab45f4eab8e712ea87ac4daeec032d2a6c2301a8ca376f070927e65e2c609890c2e0a314948d3a'
