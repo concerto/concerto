9c974bac16b65d00525bcccdb69f2a89aa397481
Brian M <michab3@rpi.edu>
2010-06-13 19:50:58 -0400

    Work on submission moderation actions and moderation state testing.

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -7,10 +7,50 @@ class Submission < ActiveRecord::Base
   validates :feed, :presence => true, :associated => true
   validates :content, :presence => true, :associated => true
   validates :moderator, :associated => true
+  validates :duration, :numericality => { :greater_than => 0 }
   validates_uniqueness_of :content_id, :scope => :feed_id  #Enforce content can only be submitted to a feed once
 
   #Scoping shortcuts for active/denied/pending
   scope :approved, where(:moderation_flag => true)
   scope :denied, where(:moderation_flag => false)
-  scope :pending, where("moderation_flag IS NULL") 
+  scope :pending, where("moderation_flag IS NULL")
+  
+  # Test if the submission has been approved.
+  # (moderation flag is true)
+  def is_approved?
+    moderation_flag ? true : false
+  end
+  
+  # Test if the submission has been denied.
+  # (moderation flag is false)
+  def is_denied?
+    (moderation_flag == false) ? true : false
+  end
+  
+  # Test if the submission has not yet been moderated.
+  # (moderation flag is nil)
+  def is_pending?
+    moderation_flag.nil?
+  end
+  
+  # Approve a piece of content on a feed.  Must be 
+  # affilailated with a moderator.  Duration can be
+  # overridden as needed.
+  def approve(moderator, duration = self.duration)
+    update_attributes({:moderation_flag => true, :duration => duration, :moderator => moderator})
+  end
+  
+  # Deny a piece of content on a feed.  Must be affiliated
+  # with a moderator.  Duration is not changed, because the
+  # the content is being denied.
+  def deny(moderator)
+    update_attributes({:moderation_flag => false, :moderator => moderator})
+  end
+  
+  # Resets the moderation state of a content submission to a feed.
+  # Must be affialiated with a moderator.  Duration resets on default
+  # to the duration specified by the uploader.
+  def unmoderate(moderator, duration = self.content.duration)
+    update_attributes({:moderation_flag => nil, :duration => duration, :moderator => moderator})
+  end
 end
--- test/unit/submission_test.rb
+++ test/unit/submission_test.rb
@@ -1,12 +1,21 @@
 require 'test_helper'
 
 class SubmissionTest < ActiveSupport::TestCase
+  # Attributes cannot be left empty/blank
+  test "submission attributes must not be empty" do
+    submission = Submission.new
+    assert submission.invalid?
+    assert submission.errors[:duration].any?
+    assert submission.errors[:feed].any?
+    assert submission.errors[:content].any?
+  end
+  
   # Every submission requires a feed
   test "submission requires feed" do
     blank = Submission.new()
     assert !blank.valid?
     
-    s = Submission.new({:content => contents(:sample_ticker)})
+    s = Submission.new({:content => contents(:futuristic_ticker), :duration => 10})
     assert !s.valid?, "Submission doesn't have feed"
     s.feed = feeds(:service)
     assert s.valid?, "Submission has feed"
@@ -14,19 +23,85 @@ class SubmissionTest < ActiveSupport::TestCase
   
   # Content is critical to a submission
   test "submission requires content" do
-    s = Submission.new({:feed => feeds(:service)})
+    s = Submission.new({:feed => feeds(:service), :duration => 10})
     assert !s.valid?, "Submission doesn't have content"
-    s.content_id = contents(:sample_ticker).id
+    s.content_id = contents(:futuristic_ticker).id
     assert s.valid?, "Submission has content"
   end
   
   # Test uniqueness of submissions, a piece of content
   # cannot be submitted to the same feed more than once
   test "submissions must be unique" do
-    s = Submission.new({:content => contents(:old_ticker), :feed => feeds(:service)})
+    s = Submission.new({:content => contents(:old_ticker), :feed => feeds(:service), :duration => 10})
     assert !s.valid?, "Submission already exists"
     s.content = contents(:futuristic_ticker)
     assert s.valid?, "Submission doesn't exist"
+  end
+  
+  # Verify is_approved? is only true for approved content
+  test "is_approved?" do
+    assert submissions(:approved_ticker).is_approved?
+    assert !submissions(:denied_ticker).is_approved?
+    assert !submissions(:pending_ticker).is_approved?
+  end
+  
+  # Verify is_denied? is only true for denied content
+  test "is_denied?" do
+    assert !submissions(:approved_ticker).is_denied?
+    assert submissions(:denied_ticker).is_denied?
+    assert !submissions(:pending_ticker).is_denied?
+  end
+  
+  # Verify is_pending? is only true for pending content
+  test "is_pending?" do
+    assert !submissions(:approved_ticker).is_pending?
+    assert !submissions(:denied_ticker).is_pending?
+    assert submissions(:pending_ticker).is_pending?
+  end
+  
+  # Verify the approve behavior approves the content.
+  # If a duration is set, make sure that is used.
+  test "submission approval" do
+    s = submissions(:pending_ticker)
+    assert !s.is_approved?
+    
+    assert s.approve(users(:katie)), "#{s.errors}"
+    assert s.is_approved?
+    assert_equal s.moderator, users(:katie)
+    
+    assert s.approve(users(:kristen), 123)
+    assert s.is_approved?
+    assert_equal s.moderator, users(:kristen)
+    assert_equal s.duration, 123
+  end
+  
+  # Make sure content can be denied.
+  test "submission denial" do
+    s = submissions(:approved_ticker)
+    assert !s.is_denied?
+    
+    assert s.deny(users(:katie)), "#{s.errors}"
+    assert s.is_denied?
+    assert_equal s.moderator, users(:katie)
+  end
+  
+  # Make sure content can be unmoderated.  If
+  # a duration is used, that one should be used
+  # otherwise it should be reset to the
+  # content's duration.
+  test "submission un-moderation" do
+    s = submissions(:denied_ticker)
+    assert !s.is_pending?
+    
+    assert s.unmoderate(users(:katie), 234), "#{s.errors}"
+    assert s.is_pending?
+    assert_equal s.moderator, users(:katie)
+    assert_equal s.duration, 234
+    
+    assert s.unmoderate(users(:katie)), "#{s.errors}"
+    assert s.is_pending?
+    assert_equal s.moderator, users(:katie)
+    assert_equal s.duration, s.content.duration
   end
 
 end
