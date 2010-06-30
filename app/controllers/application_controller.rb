c66b4d0401bb0cd682da2759f0d205ca409984ff
Brian M <michab3@rpi.edu>
2010-06-30 12:54:32 -0400

    Moving the STI content type array to the content helper.  No longer used in a controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  
-  helper_method :content_types
-  
-  CONTENT_TYPES = [Graphic, Ticker]
-  
-  # Expose the available content subclasses.
-  # Plugins will want to add their subclass to
-  # this list in somehow (we'll figure that out
-  # at a later commit)
-    def content_types
-    CONTENT_TYPES
-  end
 end
--- app/helpers/contents_helper.rb
+++ app/helpers/contents_helper.rb
@@ -1,2 +1,12 @@
 module ContentsHelper
+
+  CONTENT_TYPES = [Graphic, Ticker]
+  
+  # Expose the available content subclasses.
+  # Plugins will want to add their subclass to
+  # this list.
+  def content_types
+    CONTENT_TYPES
+  end
+
 end
