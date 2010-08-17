be0439ef884534a04adade35c1b0fe5ec39ed5a9
Brian M <michab3@rpi.edu>
2010-08-17 19:01:54 -0400

    Another attempt at a modular system to build things.  I call this system "HTML5".  See lighthouse for more details.  [#105 state:resolved]

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,16 +7,4 @@ module ApplicationHelper
     tag("div", {:class => "C-header_back"}, true) + link_to(name, path, options) + raw("</div>")
   end
 
-  # Figure out which class we should be using to setup the sidebar.
-  # The default for no sidebar is "no" but if a sidebar is used
-  # the default becomes "sm" (small) but it can be overridden by
-  # setting @sidebar_class anywhere a sidebar is used.
-  def sidebar_class
-    if content_for?(:sidebar)
-      @sidebar_class || "sm"
-    else
-      "no"
-    end
-  end
-
 end
