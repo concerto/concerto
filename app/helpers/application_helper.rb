cb6e58b75f9a2b996d658326ee63ad4fdcabac1d
Brian M <michab3@rpi.edu>
2010-08-17 11:18:08 -0400

    Back button now generated via helper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+  def back_button(path, name = "Back", options = {})
+    options[:class] ||= "button back"
+
+    tag("div", {:class => "C-header_back"}, true) + link_to(name, path, options) + raw("</div>")
+  end
+
 end
