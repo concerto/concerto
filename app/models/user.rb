a1d648a2af8a419852460ab8d8eba750df285c2b
August <august@alum.rpi.edu>
2011-05-30 20:35:25 -0400

    Fixed mass-assignment setting for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name
 
   has_many :contents
   has_many :submissions, :foreign_key => "moderator_id"
