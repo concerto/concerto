358e32e1575b990f678d014d1f95003ab856fe39
August <august@alum.rpi.edu>
2011-08-01 12:34:13 -0400

    Reorganized localization files and started in on the views...

--- config/application.rb
+++ config/application.rb
@@ -29,6 +29,7 @@ module Concerto
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     #config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
     config.i18n.default_locale = :en
 
     # JavaScript files you want as :defaults (application.js is always included).
