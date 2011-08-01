3254ed57dd09697bf76b19fd557a728e045e940b
August <august@alum.rpi.edu>
2011-07-31 20:17:38 -0400

    Added the rudiments of localization (I18n)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110614201821) do
+ActiveRecord::Schema.define(:version => 20110801000503) do
 
   create_table "contents", :force => true do |t|
     t.string   "name"
@@ -135,6 +135,7 @@ ActiveRecord::Schema.define(:version => 20110614201821) do
     t.datetime "updated_at"
     t.string   "first_name"
     t.string   "last_name"
+    t.string   "locale"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
