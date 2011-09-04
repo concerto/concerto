b9d4fa87949e1849e4686578f497ea140f21b1e5
Brian R Zaik <brian.r.zaik@gmail.com>
2011-09-04 14:26:02 -0600

    Changed registrations view and added better page title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,5 +6,12 @@ module ApplicationHelper
     options[:class] ||= "button back"
     tag("div", {:class => "C-header_back"}, true) + link_to(name, path, options) + raw("</div>")
   end
+  
+  # Generate page titles
+  def yield_for_title(content_sym, default)
+    output = content_for(content_sym)
+    output = "#{ controller.action_name.titleize } - #{ controller.controller_name.titleize }" if output.blank?
+    output
+  end
 
 end
