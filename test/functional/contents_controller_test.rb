918ae80b7bea008e01233eb012b296078681dc5e
Brian Michalski <bmichalski@gmail.com>
2011-09-10 18:33:50 -0700

    Adding some tests to the content#new process and ticker / graphic uploads.

---
+++ test/functional/contents_controller_test.rb
@@ -0,0 +1,27 @@
+require 'test_helper'
+
+class ContentsControllerTest < ActionController::TestCase
+  include Devise::TestHelpers
+
+  def setup
+    request.env["devise.mapping"] = Devise.mappings[:user]
+  end
+
+  test "should get generic new" do
+    get :new
+    assert_response :success
+  end
+
+  test "should get new graphic" do
+    get(:new, {:type => "graphic"})
+    assert_response :success
+    assert_select(HTML::Selector.new "input[type=file]")
+  end
+
+  test "should get new ticker" do
+    get(:new, {:type => "ticker"})
+    assert_response :success
+    assert_select("textarea")
+  end
+
+end
