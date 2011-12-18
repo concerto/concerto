8a87ee02208c6bc45b9476017f9aa2eac51e4ddd
Brian R Zaik <brian.r.zaik@gmail.com>
2011-12-18 01:11:18 -0700

    Brought render_content helper function over to submissions_helper and did some work on submissions#show

--- app/helpers/submissions_helper.rb
+++ app/helpers/submissions_helper.rb
@@ -1,2 +1,28 @@
 module SubmissionsHelper
+  CONTENT_TYPES = [Graphic, Ticker]
+  
+  # Expose the available content subclasses.
+  # Plugins will want to add their subclass to
+  # this list.
+  def content_types
+    CONTENT_TYPES
+  end
+  
+  # Render a piece of content.
+  #
+  # options[:type] controls what partial is used
+  # when rendering the file.  Defaults to 
+  # _render_default.html.erb (:type = 'default').
+  #
+  # All options set are passed to the partial in options.
+  # The content object is passed in content.
+  def render_content(content, options={})
+    options.symbolize_keys! #All the cool kids do this
+  
+    options[:type] ||= 'default'
+    
+    render  :partial => "contents/#{content.class.to_s.underscore}/render_#{options[:type]}", 
+            :locals => {:content => content, :options => options}
+  end
+  
 end
