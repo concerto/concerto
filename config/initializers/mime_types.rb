126cd509885e541eb125e17f22ede8625255db31
Brian Michalski <bmichalski@gmail.com>
2012-01-28 16:48:50 -0800

    Bumping to Rails 3.2.1 and Devise 2.  Closes #27.
    Devise 2.0 changes the migration syntax, which was manually adjusted.  The langauage files also changed slightly.
    
    The Rails 3.2.x upgrade threw an error about PNG already being registered, so we only define mime types if needed.

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -3,6 +3,6 @@
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
 # Mime::Type.register_alias "text/html", :iphone
-Mime::Type.register "image/jpg", :jpg
-Mime::Type.register "image/png", :png
-Mime::Type.register "image/svg+xml", :svg
+Mime::Type.register "image/jpg", :jpg unless Mime::Type.lookup_by_extension(:jpg)
+Mime::Type.register "image/png", :png unless Mime::Type.lookup_by_extension(:png)
+Mime::Type.register "image/svg+xml", :svg unless Mime::Type.lookup_by_extension(:svg)
