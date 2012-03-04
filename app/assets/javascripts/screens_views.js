2f79bcdd473bb39cfa011663d808cdc653f391d9
Brian R Zaik <brian.r.zaik@gmail.com>
2012-03-03 21:10:14 -0700

    Added JS to auto-select input inside inset-selection gridlists

---
+++ app/assets/javascripts/screens_views.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  // inset-selection gridlist: when a user clicks on an item in this
+  // type of gridlist, auto-select the input that's inside of the item
+  $("ul.GL.inset-selection li > a").click(function(e) {
+    e.preventDefault();
+    $(this).find(".inp input").prop("checked", true);
+  });
+});
