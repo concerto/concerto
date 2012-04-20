b4ccecd6986a8379ba7f0e6bc18ea72aca32eb2d
Zach Rowe <zrowe007@gmail.com>
2012-04-19 22:56:48 -0400

    adding multi-position template test for #38

--- test/unit/template_test.rb
+++ test/unit/template_test.rb
@@ -13,6 +13,24 @@ class TemplateTest < ActiveSupport::TestCase
     assert_equal 0.592, actual.right
     assert_equal 0.796, actual.bottom
   end
+
+  test "importing a template with multiple positions" do
+    t = Template.new
+    file = fixture_file_upload("/files/two_field_template.xml", 'text/xml')
+    assert t.import_xml(file.read)
+    actual = t.positions
+    # first position
+    assert_equal 0.025, actual.first.left
+    assert_equal 0.026, actual.first.top
+    assert_equal 0.592, actual.first.right
+    assert_equal 0.796, actual.first.bottom
+    # second position
+    assert_equal 0.025, actual.last.left
+    assert_equal 0.796, actual.last.top
+    assert_equal 0.592, actual.last.right
+    assert_equal 1.000, actual.last.bottom
+    
+  end
   
   # Test the ability to import a template without fields
   test "importing an empty template" do
