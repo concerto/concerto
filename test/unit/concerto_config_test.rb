7f6e694ac8383ec919b342e15807ea4952602f07
Brian Michalski <bmichalski@gmail.com>
2012-04-29 15:58:06 -0700

    Add more ConcertoConfig tests.

--- test/unit/concerto_config_test.rb
+++ test/unit/concerto_config_test.rb
@@ -6,4 +6,15 @@ class ConcertoConfigTest < ActiveSupport::TestCase
     assert_equal(ConcertoConfig[:default_upload_type], "graphic")
   end
 
+  # Finders still work
+  test "Can create or find by if needed" do
+    assert_difference('ConcertoConfig.count', 1) do
+      ConcertoConfig.find_or_create_by_key(:key => "foo", :value => "bar")
+    end
+  end
+
+  # Attribute-style syntax works
+  test "Attribute syntax" do
+    assert_equal ConcertoConfig.default_upload_type, "graphic"
+  end
 end
