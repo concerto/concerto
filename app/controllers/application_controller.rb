25bc48c5bec0a0ffaafdbc0b7cffc7c0847a616a
August <august@inniogroup.com>
2012-05-02 18:03:11 -0400

    Creating redirect for initial user creation per #65

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :set_locale
+  before_filter :check_for_initial_install
   
   def set_locale
     if user_signed_in? && current_user.locale != ""
@@ -9,4 +10,12 @@ class ApplicationController < ActionController::Base
 
     I18n.locale = session[:locale] || I18n.default_locale
   end
+
+  #If there are no users defined yet, redirect to create the first admin user
+  def check_for_initial_install
+     if User.all.empty?
+       redirect_to new_user_registration_path
+     end
+  end
+  
 end
