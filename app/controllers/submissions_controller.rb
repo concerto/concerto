7a92d05ddf79e24fe13bdd8de269e89a442256a6
Zach Rowe <zrowe007@gmail.com>
2012-05-03 00:05:18 -0400

    the current user is the moderator.  partial fix for #77

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -6,7 +6,7 @@ class SubmissionsController < ApplicationController
     @this_feed = Feed.find(params[:feed_id])
     @sub_feeds = @this_feed.children
     @submissions = Submission.where(:feed_id => params[:feed_id])
-    
+
     respond_to do |format|
       format.js { }
       format.html { }
@@ -15,7 +15,7 @@ class SubmissionsController < ApplicationController
 
   def show
     @submission = Submission.find(params[:id])
-    
+
     respond_to do |format|
       format.js { }
       format.html { }
@@ -26,19 +26,19 @@ class SubmissionsController < ApplicationController
   def approve
     submission = Submission.find(params[:id])
     respond_to do |format|
-      if submission.approve(User.find(params[:user_id]), params[:submission][:duration])
+      if submission.approve(current_user, params[:submission][:duration])
         format.html { redirect_to(feed_submissions_path, :notice => t(:content_approved)) }
       else
         format.html { redirect_to(feed_submission_path, :notice => t(:content_failed_approve)) }
       end
     end
   end
-  
+
   # PUT /feeds/1/submission/1/deny
   def deny
     submission = Submission.find(params[:id])
     respond_to do |format|
-      if submission.deny(User.find(params[:user_id]))
+      if submission.deny(current_user)
         format.html { redirect_to(feed_submissions_path, :notice => t(:content_denied)) }
       else
         logger.debug submission.errors
