0beace63ddab444664eab5f749c2b9ae9530afb9
August <august@inniogroup.com>
2012-05-05 02:23:07 -0400

    Documented and cleaned up config variables a bit

--- app/models/concerto_config.rb
+++ app/models/concerto_config.rb
@@ -1,10 +1,21 @@
 #Current configuration keys:
-#:public_concerto
-#:default_upload_type
-#:content_default_start_time
-#:content_default_end_time
-#:content_default_duration
-#:setup_complete
+  #public_concerto
+  #default_upload_type
+  #content_default_start_time
+  #content_default_end_time
+  #setup_complete
+  #default_content_run_time
+  #start_date_offset
+
+#Adding a new configuration variable:
+  #Either in your plugin, or in the seeds file, add a row as such:
+  #  ConcertoConfig.find_or_create_by_key(:key => "default_upload_type", :value => "graphic", :value_default => "graphic", :value_type => "string")
+  #The value type and default will allow the Dashboard to properly create the form for editing the variable
+  #Also ensure to provide a translation in an appropriate YAML file:
+  #  default_upload_type: "Default content upload type"
+  
+  #The variable can then be accessed like this: ConcertoConfig[:public_concerto]
+  #and modified by calling ConcertoConfig.set(public_concerto,true)
 
 class ConcertoConfig < ActiveRecord::Base
   
@@ -16,7 +27,6 @@ class ConcertoConfig < ActiveRecord::Base
 
   # Enable hash-like access to table for ease of use
   # Returns false if key isn't found
-  # Example: ConcertoConfig[:public_concerto] => "true"
   def self.[](key)
     rec = self.find_by_key(key.to_s)
     if rec.nil?
