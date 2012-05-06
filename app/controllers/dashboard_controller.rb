df36d6abcbf886907503c543753a262432394222
August <august@inniogroup.com>
2012-05-05 21:02:12 -0400

    Locked down dashboard to admins

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,12 +1,14 @@
 class DashboardController < ApplicationController
-
+  
   # GET /dashboard
   def index
+    authorize! :read, @concerto_config
     @concerto_configs = ConcertoConfig.all
   end
 
   #get a hash of concerto_config keysand values and update them using the ConcertoConfig setter
   def update
+    authorize! :update, @concerto_config
     params[:concerto_config].each  do |k,v|
       ConcertoConfig.set(k,v)
     end
