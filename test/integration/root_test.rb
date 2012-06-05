4fd84606bc5c0b8dddd8a6c5925e123ca7dbb44d
Brian Michalski <bmichalski@gmail.com>
2012-06-04 21:49:25 -0700

    Properly check for submissions#[index,show] and select the button.  Close #99.

--- test/integration/root_test.rb
+++ test/integration/root_test.rb
@@ -12,4 +12,15 @@ class RootTest < ActionController::IntegrationTest
     # And it should never ever ever redirect
     assert !redirect?
   end
+
+  test "signed in root urls load" do
+    post "/users/sign_in", :user => {:email => users(:katie).email, :password => 'katie'}
+    assert :success
+
+    get "/feeds"
+    assert :success
+
+    get "/feeds/#{feeds(:service).id}/submissions"
+    assert :success
+  end
 end
