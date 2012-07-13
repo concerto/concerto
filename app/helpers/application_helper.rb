ff0869ed848e2327787a855c3be241ed5fac8c34
Brian R Zaik <brian.r.zaik@gmail.com>
2012-07-12 20:45:56 -0600

    Fixed a CSS class issue with C-header_back

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,7 @@ module ApplicationHelper
   # in the header elements.
   def back_button(path, name = "Back", options = {})
     options[:class] ||= "button back"
-    tag("div", {:class => "C-header_back"}, true) + link_to(name, path, options) + raw("</div>")
+    tag("div", {:class => "viewblock-header_back"}, true) + link_to(name, path, options) + raw("</div>")
   end
   
   # Generate page titles.
