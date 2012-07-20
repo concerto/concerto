7d71be81ff5327c8010de8f4851d080f685ef0f5
Zach Rowe <zrowe007@gmail.com>
2012-07-20 15:31:36 -0400

    add member listing to close #148

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -1,2 +1,16 @@
 module GroupsHelper
+  def member_list( group = nil )
+    orderedMemberships = group.memberships.leader + group.memberships.regular
+    orderedMemberships.each_with_index do |membership, i|
+      concat link_to membership.user.name, membership.user
+      concat " (leader) " if membership.level == Membership::LEVELS[:leader]
+      if i == 9 && membership != orderedMemberships.last
+        concat " and "
+        concat link_to "more", group
+        break
+      elsif i != 9 && membership != orderedMemberships.last
+      	concat ", "
+      end
+    end
+  end
 end
