888a5a507e2453281085f4af18ae42e44864efe8
Brian R Zaik <brian.r.zaik@gmail.com>
2012-07-21 23:19:14 -0600

    Fixed JS ID to fix #150

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,14 +22,14 @@ $(document).ready(function () {
 	//Content grid/table switching
 	$('a.update_holder').live('click', function(event){
 		event.preventDefault();
-		target_url = $(this).attr('href')
+		target_url = $(this).attr('href');
 		$.ajax({
 			url: target_url,
 			dataType: "html",
 			cache: false,
 			success: function(data) {
 				$("#content_holder").fadeOut(100, function(){
-					$("#content_holder").html("").html(data).fadeIn('slow')
+					$("#content_holder").html("").html(data).fadeIn('slow');
 				});
 			}
 		});
@@ -47,21 +47,24 @@ $(document).ready(function () {
       $(window).bind("popstate", function () {
         $.getScript(location.href);
       });
-    })
-  }
-  
-  // flash-banner display animation
-  if ( $("#flash-banner").not(':empty') ) {
-    var topmenuHeight = $("#TM").height();
-    $("#flash-banner").animate({
-      top: '+=' + topmenuHeight
-    }, 1000, function() {
-      // first animation is complete, so move it back up after 4 seconds:
-      $("#flash-banner").delay(4000).animate({
-        top: '0'
-      }, 1000, function() {});
     });
   }
+  
+  // flash-banner display animation:
+  if ( $("#flash-banner").html() !== "" ) {
+    $(function () {
+      var topmenuHeight = $("#top-menu").height();
+      $("#flash-banner").animate({
+        top: '+=' + topmenuHeight
+      }, 1000, function() {
+        // first animation is complete, so move it back up after 4 seconds:
+        $("#flash-banner").delay(4000).animate({
+          top: '0'
+        }, 1000, function() {});
+      });
+    });
+  }
+
 });
 
 
@@ -74,4 +77,4 @@ jQuery(function($) {
       $("#owner_div").html(data);
     });
   });
-})
+});
