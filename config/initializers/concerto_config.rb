concerto/concerto

commit 6fbf9882baf510daa85ddc79f66c45514532bfdd

Config Initializer fix to ease migrating to latest db
We'll have to continue to keep an eye on database changes that might
prevent initializer from booting, otherwise db migration is problematic.

config/initializers/concerto_config.rb
@@ -1,6 +1,6 @@
 #Initialize all core Concerto Config entries
 
-if ActiveRecord::Base.connection.table_exists? 'concerto_configs'
+if ActiveRecord::Base.connection.table_exists? 'concerto_configs' && ConcertoConfig.columns_hash.has_key?("plugin_id")
   ConcertoConfig.make_concerto_config("default_upload_type", "graphic")
   ConcertoConfig.make_concerto_config("public_concerto", "true", :value_type => "boolean")
   ConcertoConfig.make_concerto_config("content_default_start_time", "12:00 am")

commit a3af68e7237268b89ea2592c1957fc61688d6ddb

Fixed logic in config initializer whose evaluation was causing new installs to break with a table not found error - introduced in 6fbf9882baf510daa85ddc79f66c45514532bfdd

config/initializers/concerto_config.rb
@@ -1,24 +1,17 @@
 #Initialize all core Concerto Config entries
 
-if ActiveRecord::Base.connection.table_exists? 'concerto_configs' && ConcertoConfig.columns_hash.has_key?("plugin_id")
-  ConcertoConfig.make_concerto_config("default_upload_type", "graphic")
-  ConcertoConfig.make_concerto_config("public_concerto", "true", :value_type => "boolean")
-  ConcertoConfig.make_concerto_config("content_default_start_time", "12:00 am")
-  ConcertoConfig.make_concerto_config("content_default_end_time", "11:59 pm")
-  ConcertoConfig.make_concerto_config("start_date_offset", "0", :value_type => "integer")
-  ConcertoConfig.make_concerto_config("default_content_run_time", "7", :value_type => "integer")
-  ConcertoConfig.make_concerto_config("setup_complete", "false", :value_type => "boolean", :value_default => "true")
-  ConcertoConfig.make_concerto_config("allow_registration", "true", :value_type => "boolean")
-  ConcertoConfig.make_concerto_config("allow_user_screen_creation", "false", :value_type => "boolean")
-  ConcertoConfig.make_concerto_config("allow_user_feed_creation", "true", :value_type => "boolean")
-  ConcertoConfig.make_concerto_config("rubygem_executable", "gem")
+if ActiveRecord::Base.connection.table_exists? 'concerto_configs'
+  if ConcertoConfig.columns_hash.has_key?("plugin_id")
+    ConcertoConfig.make_concerto_config("default_upload_type", "graphic")
+    ConcertoConfig.make_concerto_config("public_concerto", "true", :value_type => "boolean")
+    ConcertoConfig.make_concerto_config("content_default_start_time", "12:00 am")
+    ConcertoConfig.make_concerto_config("content_default_end_time", "11:59 pm")
+    ConcertoConfig.make_concerto_config("start_date_offset", "0", :value_type => "integer")
+    ConcertoConfig.make_concerto_config("default_content_run_time", "7", :value_type => "integer")
+    ConcertoConfig.make_concerto_config("setup_complete", "false", :value_type => "boolean", :value_default => "true")
+    ConcertoConfig.make_concerto_config("allow_registration", "true", :value_type => "boolean")
+    ConcertoConfig.make_concerto_config("allow_user_screen_creation", "false", :value_type => "boolean")
+    ConcertoConfig.make_concerto_config("allow_user_feed_creation", "true", :value_type => "boolean")
+    ConcertoConfig.make_concerto_config("rubygem_executable", "gem")
+  end
 end
-
-
-
-
-
-
-
-
-