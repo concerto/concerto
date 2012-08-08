b22cc1e3b074c36e0ea289587144a157e4ff9121
Brian Michalski <bmichalski@gmail.com>
2012-08-08 14:21:19 -0700

    Clean up the fix for #177.
    - Use the ability to test if a membership can be created.
    - Hide the block entirely if you aren't a member and cannot join.
    - Test it.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -156,9 +156,7 @@ class Ability
       membership.group.leaders.include?(user)
     end
     # Regular users can only create pending memberships.
-    can :create, Membership do |membership|
-      user.persisted? && membership.level == Membership::LEVELS[:pending]
-    end
+    can :create, Membership, :level => Membership::LEVELS[:pending] if user.persisted?
     # Users can delete their own memberships.
     can :destroy, Membership, :user => user
     # Group members can read all other memberships
--- test/functional/groups_controller_test.rb
+++ test/functional/groups_controller_test.rb
@@ -21,4 +21,20 @@ class GroupsControllerTest < ActionController::TestCase
     assert_equal assigns(:my_groups).count, 2
     assert_equal assigns(:groups).count, 2
   end
+
+  test "not signed in user can see public group" do
+    get :show, :id => groups(:wtg).id
+    assert_equal assigns(:group), groups(:wtg)
+    assert_response :success
+  end
+
+  test "signed in user can see their groups" do
+    sign_in users(:katie)
+    groups = [groups(:wtg), groups(:rpitv)]
+    groups.each do |g|
+      get :show, :id => g.id
+      assert_equal assigns(:group), g
+      assert_response :success
+    end
+  end
 end
