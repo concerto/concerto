f7ae0848c97f242ef3c96da54fcd43cb013016b9
Brian Michalski <bmichalski@gmail.com>
2012-08-10 22:35:51 -0700

    Partial revert "Updated install script and initializer"
    This reverts the config changes in commit 1d31ac725f48a5e5b47bdfc6d29b1f6bdd758e38.  All tests and CI suites were failing.

--- config/initializers/02-concerto_config.rb
+++ config/initializers/02-concerto_config.rb
@@ -1,12 +1,17 @@
 #Initialize all core Concerto Config entries
-ConcertoConfig.make_concerto_config("default_upload_type", "graphic")
-ConcertoConfig.make_concerto_config("public_concerto", "true", :value_type => "boolean")
-ConcertoConfig.make_concerto_config("content_default_start_time", "12:00 am")
-ConcertoConfig.make_concerto_config("content_default_end_time", "11:59 pm")
-ConcertoConfig.make_concerto_config("start_date_offset", "0", :value_type => "integer")
-ConcertoConfig.make_concerto_config("default_content_run_time", "7", :value_type => "integer")
-ConcertoConfig.make_concerto_config("setup_complete", "false", :value_type => "boolean", :value_default => "true")
-ConcertoConfig.make_concerto_config("allow_registration", "true", :value_type => "boolean")
-ConcertoConfig.make_concerto_config("allow_user_screen_creation", "false", :value_type => "boolean")
-ConcertoConfig.make_concerto_config("allow_user_feed_creation", "true", :value_type => "boolean")
-ConcertoConfig.make_concerto_config("rubygem_executable", "gem")
+
+if ActiveRecord::Base.connection.table_exists? 'concerto_configs'
+  if ConcertoConfig.columns_hash.has_key?("plugin_id")
+    ConcertoConfig.make_concerto_config("default_upload_type", "graphic")
+    ConcertoConfig.make_concerto_config("public_concerto", "true", :value_type => "boolean")
+    ConcertoConfig.make_concerto_config("content_default_start_time", "12:00 am")
+    ConcertoConfig.make_concerto_config("content_default_end_time", "11:59 pm")
+    ConcertoConfig.make_concerto_config("start_date_offset", "0", :value_type => "integer")
+    ConcertoConfig.make_concerto_config("default_content_run_time", "7", :value_type => "integer")
+    ConcertoConfig.make_concerto_config("setup_complete", "false", :value_type => "boolean", :value_default => "true")
+    ConcertoConfig.make_concerto_config("allow_registration", "true", :value_type => "boolean")
+    ConcertoConfig.make_concerto_config("allow_user_screen_creation", "false", :value_type => "boolean")
+    ConcertoConfig.make_concerto_config("allow_user_feed_creation", "true", :value_type => "boolean")
+    ConcertoConfig.make_concerto_config("rubygem_executable", "gem")
+  end
+end
