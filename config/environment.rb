904dbe247cff33de38a7f4d7954da894fe4c9740
August <august@inniogroup.com>
2012-08-10 23:14:48 -0400

    Database file placement had to be done before any user-side initializations

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,10 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+#If no database configuration exists, copy the default SQLite one over...
+unless FileTest.exists?("config/database.yml")
+  FileUtils.cp "config/database.yml.sample", "config/database.yml"
+end
+
 # Initialize the rails application
 Concerto::Application.initialize!
--- config/initializers/00-check_database_config.rb
+++
@@ -1,4 +0,0 @@
-#If not database configuration exists, copy the default SQLite one over...
-unless FileTest.exists?("config/database.yml")
-  FileUtils.cp "/config/database.yml.sample", "/config/database.yml"
-end
