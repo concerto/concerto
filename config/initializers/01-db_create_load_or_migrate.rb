26a087e6bf21f8cbb75ed82e10b9408d5db791a5
Brian Michalski <bmichalski@gmail.com>
2012-08-14 00:52:07 -0400

    Require rake more selectively.  Close #180.

--- config/initializers/01-db_create_load_or_migrate.rb
+++ config/initializers/01-db_create_load_or_migrate.rb
@@ -2,15 +2,18 @@
 #Creates and migrates the database if it doesn't yet exist
 #Implementation based on http://trevorturk.wordpress.com/2008/04/10/automatically-creating-loading-and-migrating-your-database/
 
-require 'rake'
-Concerto::Application.load_tasks
-
 begin
   current_version = ActiveRecord::Migrator.current_version
   #Grab the timestamp from each migration filename, and run max() on the resulting array
   highest_version = Dir.glob("#{Rails.root.to_s}/db/migrate/*.rb").map { |f| f.match(/\d+/).to_s.to_i}.max
-  Rake::Task["db:migrate"].invoke if current_version != highest_version
+  if current_version != highest_version
+    require 'rake'
+    Concerto::Application.load_tasks
+    Rake::Task["db:migrate"].invoke
+  end
 rescue
+  require 'rake'
+  Concerto::Application.load_tasks
   Rake::Task["db:create"].invoke
   abort 'ERROR: Database has no schema version and is not empty' unless ActiveRecord::Base.connection.tables.blank?
   #Database doesn't exist - create it and migrate it
