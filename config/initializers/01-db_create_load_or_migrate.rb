a09817bbd3104e3adbe50e22e1a5c0f00e530c2d
August <august@inniogroup.com>
2012-08-17 16:56:44 -0400

    Broke out the db:setup command into its component parts

--- config/initializers/01-db_create_load_or_migrate.rb
+++ config/initializers/01-db_create_load_or_migrate.rb
@@ -7,7 +7,9 @@ begin
   #Grab the timestamp from each migration filename, and run max() on the resulting array
   highest_version = Dir.glob("#{Rails.root.to_s}/db/migrate/*.rb").map { |f| f.match(/\d+/).to_s.to_i}.max
   if current_version == 0
-    Rake::Task["db:setup"].invoke
+    Rake::Task["db:create"].invoke
+    Rake::Task["db:migrate"].invoke
+    Rake::Task["db:seed"].invoke
   elsif current_version != highest_version && current_version != nil
     require 'rake'
     Concerto::Application.load_tasks
@@ -16,6 +18,8 @@ begin
 rescue
   require 'rake'
   Concerto::Application.load_tasks
-  Rake::Task["db:setup"].invoke
+  Rake::Task["db:create"].invoke
+  Rake::Task["db:migrate"].invoke
+  Rake::Task["db:seed"].invoke
   retry
 end
