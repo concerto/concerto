b11b0a6011fd9883c8d54c038689f8cbc5f3df89
Brian Michalski <bmichalski@gmail.com>
2012-08-26 21:51:57 -0700

    Refactor the available content types to be a config array.

--- app/helpers/contents_helper.rb
+++ app/helpers/contents_helper.rb
@@ -1,12 +1,8 @@
-module ContentsHelper
+module ContentsHelper  
 
-  CONTENT_TYPES = [Graphic, Ticker]
-  
   # Expose the available content subclasses.
-  # Plugins will want to add their subclass to
-  # this list.
   def content_types
-    CONTENT_TYPES
+    Concerto::Application.config.content_types
   end
 
   # Render a piece of content.
---
+++ config/initializers/11-content_types.rb
@@ -0,0 +1,3 @@
+# An array of all the possible content types.
+# Plugins will want to append their classes to this list.
+Concerto::Application.config.content_types = [Ticker, Graphic]
