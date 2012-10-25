f70679bbf33b4ce2e433b9e6f2bae88a1d2f36ff
Zach Rowe <zrowe007@gmail.com>
2012-10-25 00:59:03 -0400

    be more selective about which locales we load

--- config/application.rb
+++ config/application.rb
@@ -35,7 +35,7 @@ module Concerto
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     #config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    #config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '{controllers,views/*}', '*.{rb,yml}')]
     config.i18n.default_locale = :en
 
     # Configure the default encoding used in templates for Ruby 1.9.
