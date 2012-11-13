85637a995a5ae3772574683b2d036a819ae41893
Brian Michalski <bmichalski@gmail.com>
2012-11-12 20:30:22 -0800

    Tabs to spaces.

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,18 +1,18 @@
 class Subscription < ActiveRecord::Base
-	# Weight Levels
-	WEIGHTS = {
-		# A very frequent chance of content showing up.
-		:"very frequent" => 5,
-		# A frequent chance of content showing up.
-		:frequent => 4,
-		# Neither a frequent, nor a infrequence chance of
-		# content showing up.
-		:netural => 3,
-		# An infrequent chance of content showing up.
-		:infrequent => 2,
-		# A very infrequent chance of content showing up.
-		:"very infrequent" => 1,
-	}
+  # Weight Levels
+  WEIGHTS = {
+    # A very frequent chance of content showing up.
+    :"very frequent" => 5,
+    # A frequent chance of content showing up.
+    :frequent => 4,
+    # Neither a frequent, nor a infrequence chance of
+    # content showing up.
+    :netural => 3,
+    # An infrequent chance of content showing up.
+    :infrequent => 2,
+    # A very infrequent chance of content showing up.
+    :"very infrequent" => 1,
+  }
   # Associations
   belongs_to :feed
   belongs_to :field
@@ -22,8 +22,8 @@ class Subscription < ActiveRecord::Base
   validates :feed, :presence => true, :associated => true
   validates_uniqueness_of :feed_id, :scope => [:screen_id, :field_id]
 
-	#Get weight name of a subscription
-	def weight_name
-		name = (Subscription::WEIGHTS.respond_to?(:key) ? Subscription::WEIGHTS.key(weight) :  Subscription::WEIGHTS.index(weight)).to_s
-	end
+  #Get weight name of a subscription
+  def weight_name
+    name = (Subscription::WEIGHTS.respond_to?(:key) ? Subscription::WEIGHTS.key(weight) :  Subscription::WEIGHTS.index(weight)).to_s
+  end
 end
