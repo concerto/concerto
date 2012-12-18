07882535e05d76faca73d80cd3784fb58fbc4567
August <august@inniogroup.com>
2012-12-18 01:28:54 -0500

    Changed version gets to concerto-signage to get around GitHub api shittiness

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -13,7 +13,14 @@ class DashboardController < ApplicationController
         @delayed_job_running = true
       end
     end
-      
+    
+    version_query = latest_version()
+    if version_query == -1
+      @latest_version = "999"
+    else
+      @latest_version = version_query
+    end
+    
     @concerto_configs = ConcertoConfig.where("hidden IS NULL")
   end
 
@@ -25,5 +32,44 @@ class DashboardController < ApplicationController
     end
     render :action => :index
   end
+  
+  def latest_version
+    require 'open-uri'
+    begin
+      file = open('http://dl.concerto-signage.org/version.txt')
+      version = file.read.chomp!
+    rescue OpenURI::HTTPError
+      version = gh_latest_version()
+    end   
+    return version
+  end  
+    
+  def gh_latest_version
+    require 'net/https'
+    require 'uri'
+    require 'json'
+    
+    uri = URI.parse('https://api.github.com/repos/concerto/concerto/git/refs/tags')
+    http = Net::HTTP.new(uri.host, uri.port)
+    if uri.scheme == "https" # enable SSL/TLS
+      http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+      http.ca_file = Rails.root.join('config', 'cacert.pem').to_s
+    end
+    http.start {
+      http.request_get(uri.path) {|res|
+        @versions = Array.new
+        begin
+          JSON.parse(res.body).each do |tag|
+            @versions << tag['ref'].gsub(/refs\/tags\//,'')
+          end
+        rescue TypeError
+          return -1
+        end
+        @versions.sort! {|x,y| y <=> x }
+        return @versions[0]
+      }
+    }
+  end  
   
 end
