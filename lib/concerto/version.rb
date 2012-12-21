0dc71dfa3a5d20cfd67854c740fcb9a45b7f0063
Brian Michalski <bmichalski@gmail.com>
2012-12-20 19:36:28 -0800

    Bumping to 0.0.2.alphamoose.
    Also including a compiled version of the frontend.

--- lib/concerto/version.rb
+++ lib/concerto/version.rb
@@ -2,8 +2,8 @@ module Concerto
   module VERSION
     MAJOR = 0
     MINOR = 0
-    TINY = 1
-    PRE = 'alpha'
+    TINY = 2
+    PRE = 'alphamoose'
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
   end
