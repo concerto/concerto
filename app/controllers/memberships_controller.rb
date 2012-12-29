concerto/concerto

commit 1c35569375ad124e6703d2780f335feb0e7c3423

Changed group edit redirects and added ability to add any user to group - closes #237 and closes #267

app/controllers/memberships_controller.rb
@@ -18,7 +18,7 @@ class MembershipsController < ApplicationController
 
     respond_to do |format|
       if @membership.save
-        format.html { redirect_to(@group, :notice => t(:membership_created)) }
+        format.html { redirect_to({:controller => :groups, :action => :edit, :id => @group}, :notice => t(:membership_created)) }
         format.xml  { render :xml => @group, :status => :created, :location => @group }
       else
         format.html { redirect_to @group }
@@ -34,7 +34,7 @@ class MembershipsController < ApplicationController
 
     respond_to do |format|
       if (@membership.can_resign_leadership?) && (@membership.update_attributes(params[:membership]))
-        format.html { redirect_to(@group, :notice => t(:membership_updated)) }
+        format.html { redirect_to({:controller => :groups, :action => :edit, :id => @group}, :notice => t(:membership_updated)) }
         format.xml  { head :ok }
       else
         format.html { redirect_to @group , :notice => @group.errors}
@@ -50,7 +50,7 @@ class MembershipsController < ApplicationController
 
     respond_to do |format|
 			if (@membership.can_resign_leadership?) && (@membership.destroy)
-				format.html { redirect_to(@group, :notice => t(:member_removed)) }
+				format.html { redirect_to({:controller => :groups, :action => :edit, :id => @group}, :notice => t(:member_removed)) }
 				format.xml  { head :ok }
 			else
 				format.html { redirect_to @group, :notice => t(:membership_denied) }
@@ -64,9 +64,9 @@ class MembershipsController < ApplicationController
     membership = Membership.find(params[:id])
     respond_to do |format|
       if membership.approve()
-        format.html { redirect_to(group_path(params[:group_id]), :notice => t(:membership_approved)) }
+        format.html { redirect_to(edit_group_path(params[:group_id]), :notice => t(:membership_approved)) }
       else
-        format.html { redirect_to(group_path(params[:group_id]), :notice => t(:membership_denied)) }
+        format.html { redirect_to(edit_group_path(params[:group_id]), :notice => t(:membership_denied)) }
       end
     end
   end

app/models/group.rb
@@ -24,6 +24,16 @@ class Group < ActiveRecord::Base
     end
   end
 
+  #Deliver a list of only users not currently in the group
+  #Used for adding new users to a group and avoiding duplication
+  def users_not_in_group
+    users = User.all
+    self.memberships.each do |m|
+      users.delete_if {|key,value| key.id == m.user_id}
+    end
+    return users
+  end
+
   # Test if a user is part of this group
   def has_member?(user)
     users.include?(user)

app/views/groups/_edit_memberships.html.erb
@@ -91,6 +91,16 @@
   </div>
 <% end %>
 
+<% users_not_in_group = @group.users_not_in_group %>
+<% unless users_not_in_group.empty? %>
+  <h3 class="default-padding">Add Users to Group</h3>
+  <%= form_for([@group, Membership.new]) do |f| %>
+    <%= f.label :name %><br />
+    <%= f.collection_select(:user_id, users_not_in_group, :id, :name) %>
+    <%= hidden_field_tag 'autoconfirm' %>
+    <%= f.submit "Add Member", :class => "btn primary" %>
+  <% end %>
+<% end %>
 
 <h3 class="default-padding">Group Leaders</h3>
 

commit e9fcc646c0da74f4e8f64a6a14241b7de069dbea

Fix build broken by 1c35569.

app/controllers/memberships_controller.rb
@@ -18,7 +18,7 @@ class MembershipsController < ApplicationController
 
     respond_to do |format|
       if @membership.save
-        format.html { redirect_to({:controller => :groups, :action => :edit, :id => @group}, :notice => t(:membership_created)) }
+        format.html { redirect_to(edit_group_path(@group), :notice => t(:membership_created)) }
         format.xml  { render :xml => @group, :status => :created, :location => @group }
       else
         format.html { redirect_to @group }
@@ -34,7 +34,7 @@ class MembershipsController < ApplicationController
 
     respond_to do |format|
       if (@membership.can_resign_leadership?) && (@membership.update_attributes(params[:membership]))
-        format.html { redirect_to({:controller => :groups, :action => :edit, :id => @group}, :notice => t(:membership_updated)) }
+        format.html { redirect_to(edit_group_path(@group), :notice => t(:membership_updated)) }
         format.xml  { head :ok }
       else
         format.html { redirect_to @group , :notice => @group.errors}

test/functional/memberships_controller_test.rb
@@ -15,7 +15,7 @@ class MembershipsControllerTest < ActionController::TestCase
     actual = assigns(:membership)
     group = assigns(:group)
     assert_equal(Membership::LEVELS[:pending], actual.level)
-    assert_redirected_to group_path(group)
+    assert_redirected_to edit_group_path(group)
   end
 
   test "should autoaprove members added by admins" do
@@ -26,7 +26,7 @@ class MembershipsControllerTest < ActionController::TestCase
     actual = assigns(:membership)
     group = assigns(:group)
     assert_equal(Membership::LEVELS[:regular], actual.level)
-    assert_redirected_to group_path(group)
+    assert_redirected_to edit_group_path(group)
   end
 
 end