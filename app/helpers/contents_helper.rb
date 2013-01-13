9625c40cae18c5f0d6b97e8732f8b2b623a8ebc0
Brian Michalski <bmichalski@gmail.com>
2013-01-13 01:37:26 -0800

    Invert hash filtering syntax to work around 1.8.x vs 1.9.x change.
    In 1.8.x, Hash#select returns an array of [key, value] instead of a hash {:key => value} like 1.9.x.  Luckily the reject returns remained the same.

--- app/helpers/contents_helper.rb
+++ app/helpers/contents_helper.rb
@@ -18,7 +18,7 @@ module ContentsHelper
   # @param [Array<Feed>] feeds Array of feeds to find the content types from.
   # @return [Array<String>] A list of content type class names, like ["Graphic", "Ticker"].
   def allowed_content_types(feeds)
-    feeds.map {|f| f.content_types.select{|k, value| value == "1"}.keys}.flatten.uniq
+    feeds.map {|f| f.content_types.reject{|k, value| value != "1"}.keys}.flatten.uniq
   end
 
   # Render a piece of content.
