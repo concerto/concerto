6ee452f43b7f40e4bd54275eee9a3235c82fdd5f
Brian Michalski <bmichalski@gmail.com>
2013-01-14 21:49:53 -0800

    Don't init plugins if the table is missing.
    Another step on #280, the app needs to initailize regardless of this functionality so users can manually migrate if necessary.

--- config/initializers/12-plugins.rb
+++ config/initializers/12-plugins.rb
@@ -1,27 +1,24 @@
 # Concerto Plugins Initializer
+if ActiveRecord::Base.connection.table_exists? 'concerto_plugins'
+  # First register any plugin configs.
+  # This will cause ConcertoPlugin to call the ConcertoConfig
+  # class for all of the enabled plugins.
+  ConcertoPlugin.make_plugin_configs
 
-# This initializer assumes that all gems have been loaded
-# and that the database is fully migrated and ActiveRecord
-# is connected to it.
+  # Next run any boot-time initialization code that the
+  # plugins might need.
+  ConcertoPlugin.initialize_plugins
 
-# First register any plugin configs.
-# This will cause ConcertoPlugin to call the ConcertoConfig
-# class for all of the enabled plugins.
-ConcertoPlugin.make_plugin_configs
-
-# Next run any boot-time initialization code that the
-# plugins might need.
-ConcertoPlugin.initialize_plugins
-
-# Mount all the engines at their requested mount points.
-# In the future, this may perform more strict checking to
-# make sure that the plugin is enabled and the app exists.
-Rails.application.routes.append do
-  mount_points = ConcertoPlugin.get_mount_points 
-  if  mount_points.is_a? Array 
-    mount_points.each do |app|
-      mount app[:rack_app] => "/" + app[:url_string]
-      # e.g. mount ConcertoHardware::Engine => "/hardware"
+  # Mount all the engines at their requested mount points.
+  # In the future, this may perform more strict checking to
+  # make sure that the plugin is enabled and the app exists.
+  Rails.application.routes.append do
+    mount_points = ConcertoPlugin.get_mount_points 
+    if  mount_points.is_a? Array 
+      mount_points.each do |app|
+        mount app[:rack_app] => "/" + app[:url_string]
+        # e.g. mount ConcertoHardware::Engine => "/hardware"
+      end
     end
   end
 end
