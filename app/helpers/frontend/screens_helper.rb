4ba10a6c37debe9fb8153c7e8fc2b22de0db0b1d
Brian Michalski <bmichalski@gmail.com>
2013-02-07 22:17:05 -0800

    Don't use digested frontends, closes #287.
    By not using digested JavaScript for the frontend, we aim to stun sprokets and load the JS without hitting the assets directory.

--- app/helpers/frontend/screens_helper.rb
+++ app/helpers/frontend/screens_helper.rb
@@ -1,7 +1,7 @@
 module Frontend::ScreensHelper
 
   def frontend_js_path(source)
-    asset_paths.compute_public_path(source, 'frontend_js')
+    asset_paths.compute_public_path(source, 'frontend_js', :digest => false)
   end
 
 end
