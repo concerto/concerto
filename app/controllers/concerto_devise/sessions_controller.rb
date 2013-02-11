22b0816678274fca057c42a2a78dc2fab7b5c4c8
Zach Rowe <zrowe@waterfallfms.com>
2013-02-11 00:13:08 -0500

    add strong params for sessions #82

--- app/controllers/concerto_devise/sessions_controller.rb
+++ app/controllers/concerto_devise/sessions_controller.rb
@@ -1,11 +1,11 @@
 #Overriding the Devise Sessions controller for fun and profit
 class ConcertoDevise::SessionsController < Devise::SessionsController
-  
+
   # GET /resource/sign_in
   def new
-    resource = build_resource(nil)
+    resource = build_resource(nil, :unsafe => true)
     clean_up_passwords(resource)
-    
+
     # if :no_content_cell param is set to true, do not render the layout or the content cell container within the view
     if params[:no_content_cell]
       respond_with(resource) do |format|
@@ -14,7 +14,7 @@ class ConcertoDevise::SessionsController < Devise::SessionsController
     else
       respond_with (resource)
     end
-    
+
   end
-  
+
 end
