0a6a883af4af12be702eb1ca9c2ea08efcb0c7d5
Brian Michalski <bmichalski@gmail.com>
2013-02-11 20:44:48 -0800

    Airbrake needs config table to exist.

--- config/initializers/13-airbrake.rb
+++ config/initializers/13-airbrake.rb
@@ -3,22 +3,22 @@ concerto_base_config = YAML.load_file("./config/concerto.yml")
 
 if ActiveRecord::Base.connection.table_exists? 'concerto_configs'
   ConcertoConfig.make_concerto_config("send_errors", "#{concerto_base_config['airbrake_enabled_initially'].to_s}", :value_type => "boolean")
-end
 
-if defined?(Airbrake)
-  Airbrake.configure do |config|
-    def config.api_key
-      if ConcertoConfig[:send_errors] == "true"
-        return '52adf2979c2ab87c634612bef9deaaf2'
-      else 
-        return nil
+  if defined?(Airbrake)
+    Airbrake.configure do |config|
+      def config.api_key
+        if ConcertoConfig[:send_errors] == "true"
+          return '52adf2979c2ab87c634612bef9deaaf2'
+        else 
+          return nil
+        end
       end
-    end
-    config.async = (RUBY_VERSION.to_f > 1.8)
-    config.user_attributes = []
-    config.secure = true
+      config.async = (RUBY_VERSION.to_f > 1.8)
+      config.user_attributes = []
+      config.secure = true
 
-    # Uncomment the following to start reporting development mode errors.
-    #config.development_environments = []
+      # Uncomment the following to start reporting development mode errors.
+      #config.development_environments = []
+    end
   end
 end
