6f0df621b19c3b05ffcaccf5613fa8f5a97e9b59
August <august@inniogroup.com>
2013-02-26 13:08:53 -0500

    Added method to check for group leadership - closes #306

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,6 +11,15 @@ module ApplicationHelper
   def yield_for_title(default)
     content_for?(:title) ? content_for(:title) : default
   end
+  
+  def user_leads_a_group?
+    current_user.memberships.each do |m|
+      if m.level == Membership::LEVELS[:leader]
+        return true
+      end
+    end
+    return false
+  end
 
   # Render the partial at the specified path if it exists within
   # the lookup_context, otherwise render the partial specified in default
