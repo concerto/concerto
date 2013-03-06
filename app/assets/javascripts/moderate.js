1bec90692bf728f7bcfc75d768f7c7b3f6a672d4
Brian R Zaik <brian.r.zaik@gmail.com>
2013-03-05 23:55:20 -0700

    Added state toggle menu to browse pages to close #207

--- app/assets/javascripts/moderate.js
+++ app/assets/javascripts/moderate.js
@@ -1,37 +1,6 @@
 function addModerateUi(){
   $('.dropdown-control').click(function(event) { event.preventDefault(); });
 
-  $('.dropdown-control.dd-jumpto').each(function() {
-    $(this).qtip( {
-      content: {
-        text: $( $(this).attr('rel') ).html(),
-        title: {
-          text: $(this).attr('title'),
-          button: true
-        }
-      },
-      position: {
-          at: 'bottom center', // Position the tooltip above the link
-          my: 'top left',
-          viewport: $(window) // Keep the tooltip on-screen at all times
-        },
-        show: {
-          event: 'click', // Show it on click...
-          solo: true // ...and hide all other tooltips...
-        },
-        events: {
-          // this is used to highlight the first input in the box when it is shown...
-          show: function() {
-            setTimeout(function() {
-              $('.ui-tooltip-content input:first').focus(); }, 50);
-              initFeedFilters();
-            }
-        },
-        hide: 'unfocus',
-        style: 'qtip-light qtip-shadow qtip-rounded'
-      });
-  });
-
   $('.dropdown-control.dd-moderate').each(function() {
     $(this).qtip( {
       content: {
---
+++ app/assets/javascripts/submissions.js
@@ -0,0 +1,50 @@
+function addBrowseUi(){
+  $('.dropdown-control').click(function(event) { event.preventDefault(); });
+
+  $('.dropdown-control.dd-jumpto').each(function() {
+    $(this).qtip( {
+      content: {
+        text: $( $(this).attr('rel') ).html(),
+        title: {
+          text: $(this).attr('title'),
+          button: true
+        }
+      },
+      position: {
+          at: 'bottom center', // Position the tooltip above the link
+          my: 'top left',
+          viewport: $(window) // Keep the tooltip on-screen at all times
+        },
+        show: {
+          event: 'click', // Show it on click...
+          solo: true // ...and hide all other tooltips...
+        },
+        events: {
+          // this is used to highlight the first input in the box when it is shown...
+          show: function() {
+            setTimeout(function() {
+              $('.ui-tooltip-content input:first').focus(); }, 50);
+              initFeedFilters();
+            }
+        },
+        hide: 'unfocus',
+        style: 'qtip-light qtip-shadow qtip-rounded'
+      });
+  });
+
+  $("a#browse-filter-toggle").click(function(e) {
+    e.preventDefault();
+    $(this).fadeOut("fast", function() {
+      $("#browse-filter-select").fadeIn("fast");
+    });
+    
+  });
+
+}
+
+function initBrowse() {
+  addBrowseUi();
+}
+
+$(document).ready(initBrowse);
+$(document).on('page:change', initBrowse);
