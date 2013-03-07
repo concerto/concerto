cedbcdc266adadb77ac434cdc68c50cded366ce8
Brian Michalski <bmichalski@gmail.com>
2013-03-07 14:46:33 -0800

    Bump to 0.0.3.bravochicken.

--- lib/concerto/version.rb
+++ lib/concerto/version.rb
@@ -2,8 +2,8 @@ module Concerto
   module VERSION
     MAJOR = 0
     MINOR = 0
-    TINY = 2
-    PRE = 'alphamoose'
+    TINY = 3
+    PRE = 'bravochicken'
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
   end
