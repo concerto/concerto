323cbc263e99b3f2e665a361e8be2a321e783a0d
August <august@inniogroup.com>
2013-03-10 23:53:11 -0400

    Trying delayed_job without checking for ENOPERM error

--- config/initializers/14-delayed_job.rb
+++ config/initializers/14-delayed_job.rb
@@ -3,6 +3,7 @@ Rails.logger.debug "Starting 14-delayed_job.rb at #{Time.now.to_s}"
 #NB: This code will not work if more than one worker process is being used
 #Additionally, the -m argument can be used to spawn a monitor process alongside the daemon(s)
 unless Rails.env.test?
+  DELAYED_JOB_PID_PATH = "#{Rails.root}/tmp/pids/delayed_job.pid"
   
   def start_delayed_job
     Thread.new do 
@@ -11,14 +12,14 @@ unless Rails.env.test?
   end
   
   def daemon_is_running?
-    FileTest.exists?(Rails.root + "/tmp/pids/delayed_job.pid")
+    pid = File.read(DELAYED_JOB_PID_PATH).strip
+    Process.kill(0, pid.to_i)
+    true
+  rescue Errno::ENOENT, Errno::ESRCH
+    false
   end
   
-  if ConcertoConfig[:autostart_delayed_job] == "true"
-    unless daemon_is_running?
-      start_delayed_job 
-    end
-  end
+  start_delayed_job unless daemon_is_running?
 
 end
 Rails.logger.debug "Completed 14-delayed_job.rb at #{Time.now.to_s}"
