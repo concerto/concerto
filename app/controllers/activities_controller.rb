77d6ba392e6ab598574f8e689d72a7ddafe85c1f
August <august@inniogroup.com>
2013-03-11 16:02:16 -0400

    At bamnet's suggestion, this arel syntax seems to work nicely

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -6,7 +6,7 @@ class ActivitiesController < ApplicationController
       @recipient = PublicActivity::Activity.where(:recipient_id => current_user.id, :recipient_type => 'User').limit(25)
       
       #Select the activities that involve a group as the recipient for which the user is a member
-      @group_member = PublicActivity::Activity.where("recipient_id IN (#{current_user.group_ids.join(", ")}) AND recipient_type = 'Group'").limit(10)
+      @group_member = PublicActivity::Activity.where(:recipient_id => current_user.group_ids, :recipient_type => "Group").limit(10)
         
       #Select activities with neither an owner nor a recipient (public activities) - the actual owner is set in the parameters hash for these
       @public_activities = PublicActivity::Activity.where(:owner_id => nil, :recipient_id => nil).limit(10)
