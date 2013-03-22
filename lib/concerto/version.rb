d375126fcaa855cd30e173c5d16b6836ecd795bd
Brian Michalski <bmichalski@gmail.com>
2013-03-21 20:33:19 -0700

    Bump to 0.1.0.charliehorse.
    Also update the compiled frontend JS.

--- lib/concerto/version.rb
+++ lib/concerto/version.rb
@@ -1,9 +1,9 @@
 module Concerto
   module VERSION
     MAJOR = 0
-    MINOR = 0
-    TINY = 3
-    PRE = 'bravochicken'
+    MINOR = 1
+    TINY = 0
+    PRE = 'charliehorse'
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
   end
