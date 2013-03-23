ecad167da839de92b34d5ed4a427ec65ed456b52
Mike <mikldt@gmail.com>
2013-03-22 20:57:05 -0400

    Fix ConcertoPlugin engine detection - it really works!

--- app/models/concerto_plugin.rb
+++ app/models/concerto_plugin.rb
@@ -173,21 +173,20 @@ private
   # gem, and matching it against the list of available engines.
   # Returns nil if no engine is not found.
   def find_engine
+    result = nil
     # We already know the name of the gem from user input
     if Gem.loaded_specs.has_key? gem_name
       # Let's get the gem's full path in the filesystem
       gpath = Gem.loaded_specs[gem_name].full_gem_path
       # Then match the path we've got to the path of an engine - 
       #    which should have its Module Name (aka paydirt)
-      Rails::Application::Railties.engines do |engine| 
+      Rails::Application::Railties.engines.each do |engine| 
         if engine.class.root.to_s == gpath
           # Get the class name from the engine hash
-          result = engine.class.name
+          result = engine.class
           break
         end
       end
-    else # Gem not loaded
-      result = nil
     end
     result
   end
