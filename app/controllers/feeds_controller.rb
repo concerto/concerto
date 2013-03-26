296205ab4ae707061abcdd7dd52f328334ffce67
Brian Michalski <bmichalski@gmail.com>
2013-03-25 18:43:15 -0700

    Improve PublicActivity compatibility implementation w/ 1.8.

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -118,20 +118,21 @@ class FeedsController < ApplicationController
 private
 
   def get_activities
-    if current_user && RUBY_VERSION >= "1.9"
+    @activities = []
+    if current_user && defined? PublicActivity::Activity
       #Retrieve the activities for which the current user is an owner or recipient (making sure the STI field specifies user as the Type)       
-      @owner = PublicActivity::Activity.where(:owner_id => current_user.id, :owner_type => 'User').limit(25)  
-      @recipient = PublicActivity::Activity.where(:recipient_id => current_user.id, :recipient_type => 'User').limit(25)
+      owner = PublicActivity::Activity.where(:owner_id => current_user.id, :owner_type => 'User').limit(25)  
+      recipient = PublicActivity::Activity.where(:recipient_id => current_user.id, :recipient_type => 'User').limit(25)
       
       #Select the activities that involve a group as the recipient for which the user is a member
-      @group_member = PublicActivity::Activity.where(:recipient_id => current_user.group_ids, :recipient_type => "Group").limit(10)
+      group_member = PublicActivity::Activity.where(:recipient_id => current_user.group_ids, :recipient_type => "Group").limit(10)
         
       #Select activities with neither an owner nor a recipient (public activities) - the actual owner is set in the parameters hash for these
-      @public_activities = PublicActivity::Activity.where(:owner_id => nil, :recipient_id => nil).limit(10)
+      public_activities = PublicActivity::Activity.where(:owner_id => nil, :recipient_id => nil).limit(10)
       
-      @activities = @owner + @recipient + @group_member + @public_activities
+      @activities = owner + recipient + group_member + public_activities
       @activities.sort! { |a,b| b.created_at <=> a.created_at }
-    end  
+    end
   end
 
   def feed_params
