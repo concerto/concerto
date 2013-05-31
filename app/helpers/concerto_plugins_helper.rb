8a120d5a9f17f4260117dc444412e554d03f1f9b
Marvin Frederickson <marvin.frederickson@gmail.com>
2013-05-31 10:31:01 -0800

    get localized source

--- app/helpers/concerto_plugins_helper.rb
+++ app/helpers/concerto_plugins_helper.rb
@@ -2,4 +2,11 @@ module ConcertoPluginsHelper
   def plugin_sources
     t('concerto_plugins.sources').map { |key, value| [value, key] }
   end
+
+  def plugin_source(source)
+    t('concerto_plugins.sources')[source.to_sym]
+  rescue
+    # return raw value if someone messed up the localization entry
+    source
+  end
 end
