concerto/concerto

commit ff0f7cc40aed752580c0483ed9083ed1727e7b5b

Wiring and routing for v1-style screen urls per #496.

config/routes.rb
@@ -1,7 +1,19 @@
+class LegacyRouteMatcher
+  # Catch requests that have a 'mac' parameter.
+  def matches?(request)
+    return !request.query_parameters[:mac].nil?
+  end
+end
+
 Concerto::Application.routes.draw do
+  v1_router = LegacyRouteMatcher.new
+  match '/' => 'frontend/screens#index', :constraints => v1_router
+  match '/screen' => 'frontend/screens#index', :constraints => v1_router
+  root :to => 'feeds#index'
+
   resources :concerto_plugins
   match 'concerto_plugins/restart_for_plugin' => 'concerto_plugins#restart_for_plugin', :via => "post"
-  
+
   resources :activities
 
   #Custom route for the screen creation/admin form JS
@@ -13,7 +25,7 @@ Concerto::Application.routes.draw do
   # about what you are doing because they could break things in
   # a very visible way.
   namespace :frontend do
-    resources :screens, :only => [:show], :path => '' do
+    resources :screens, :only => [:show, :index], :path => '' do
       member do
         get :setup
       end
@@ -146,8 +158,6 @@ Concerto::Application.routes.draw do
   # here at the bottom to avoid capturing any of the restful content paths.
   match 'content/' => 'feeds#index'
   match 'browse/' => 'feeds#index'
-
-  root :to => 'feeds#index'
 
   # See how all your routes lay out with "rake routes"
 

app/controllers/frontend/screens_controller.rb
@@ -22,6 +22,21 @@ class Frontend::ScreensController < ApplicationController
       end
     end
   end
+  
+  # GET /frontend
+  # Handles legacy screens and stuff that doesn't know their id.
+  def index
+    if params[:mac]
+      screen = Screen.find_by_mac(params[:mac])
+      if screen
+        redirect_to frontend_screen_path(screen), :status => :moved_permanently
+      else
+        render :text => "Screen not found.", :status => 404
+      end
+    else
+      render :text => 'Bad request.', :status => 400
+    end  
+  end
 
   # GET /frontend/1/setup.json
   # Get information required to setup the screen

app/models/screen.rb
@@ -68,5 +68,14 @@ class Screen < ActiveRecord::Base
   def is_offline?
     frontend_updated_at.nil? || frontend_updated_at < (Clock.time - Screen::OFFLINE_THRESHOLD)
   end
+
+  def self.find_by_mac(mac_addr)
+    begin
+      screen = Screen.find(mac_addr)
+      return screen
+    rescue ActiveRecord::ActiveRecordError
+      return nil
+    end
+  end
 end
 

test/functional/frontend/screens_controller_test.rb
@@ -34,4 +34,8 @@ class Frontend::ScreensControllerTest < ActionController::TestCase
     assert_equal ActiveSupport::JSON.decode(@response.body), {}
   end
 
+  test "v1 redirects to screen" do
+    get(:index, {:mac => screens(:one).id})
+    assert_redirected_to frontend_screen_path(screens(:one))
+  end
 end

test/integration/routes_test.rb
@@ -18,4 +18,13 @@ class RoutesTest < ActionController::IntegrationTest
   test "no screen path in frontend route" do
     assert_recognizes({:controller => 'frontend/screens', :action => 'show', :id => '1'}, "frontend/1")
   end
+
+  test "root url correct" do
+    assert_recognizes({:controller => 'feeds', :action => 'index'}, '/')
+  end
+
+  test "v1 screen urls work" do
+    assert_recognizes({:controller => 'frontend/screens', :action => 'index'}, "?mac=123")
+    assert_recognizes({:controller => 'frontend/screens', :action => 'index'}, "screen?mac=123")
+  end
 end

commit 89abd640345d96bbc273074c9a58b523a814391d

Fix route regression introduced in ff0f7cc40aed752580c0483ed9083ed1727e7b5b per #514

config/routes.rb
@@ -6,6 +6,18 @@ class LegacyRouteMatcher
 end
 
 Concerto::Application.routes.draw do
+
+  resources :screens do
+    resources :fields, :only => [] do
+      resources :subscriptions do
+        collection do
+          get :manage
+          put :save_all
+        end
+      end
+    end
+  end
+
   v1_router = LegacyRouteMatcher.new
   match '/' => 'frontend/screens#index', :constraints => v1_router
   match '/screen' => 'frontend/screens#index', :constraints => v1_router
@@ -55,17 +67,6 @@ Concerto::Application.routes.draw do
     end
     collection do
       post :import
-    end
-  end
-
-  resources :screens do
-    resources :fields, :only => [] do
-      resources :subscriptions do
-        collection do
-          get :manage
-          put :save_all
-        end
-      end
     end
   end
   