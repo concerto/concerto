52d46ee1392bad39057e67a2998d475e6ea631bc
August <august@inniogroup.com>
2013-06-13 16:28:39 -0400

    Updated overridden Devise controllers to be in sync with new code, but signup is still broken

--- app/controllers/concerto_devise/registrations_controller.rb
+++ app/controllers/concerto_devise/registrations_controller.rb
@@ -1,7 +1,9 @@
 #Overriding the Devise Registrations controller for fun and profit
 class ConcertoDevise::RegistrationsController < Devise::RegistrationsController
+  prepend_before_filter :require_no_authentication, :only => [ :new, :create, :cancel ]
+  prepend_before_filter :authenticate_scope!, :only => [:edit, :update, :destroy]
   rescue_from ActionView::Template::Error, :with => :precompile_error_catch
-  before_filter :check_permissions, :only=>[:new, :create]
+  before_filter :check_permissions, :only=>[:new, :create]  
 
   def check_permissions
     authorize! :create, User
@@ -9,22 +11,23 @@ class ConcertoDevise::RegistrationsController < Devise::RegistrationsController
 
   # GET /resource/sign_up
   def new
-    resource = build_resource({})
+    build_resource({})
     render_registration_form(resource)
   end
 
+  # POST /resource
   def create
-    build_resource
-
+    self.resource = build_resource(sign_up_params)
+    
     # If there are no users, the first one created will be an admin
     if !User.exists?
       first_user_setup = true
       resource.is_admin = true
       # At first registration, the admin is given the option to 
       # opt-out of error reporting.
-    end
+    end    
 
-    if resource.save    
+    if resource.save
       if ConcertoConfig["setup_complete"] == false
         ConcertoConfig.set("setup_complete", "true")
         # send_errors option is displayed in the form for first setup only
@@ -35,10 +38,10 @@ class ConcertoDevise::RegistrationsController < Devise::RegistrationsController
         group = Group.find_or_create_by_name(:name => "Concerto Admins")
         Membership.create(:user_id => resource.id, :group_id => group.id, :level => Membership::LEVELS[:leader])
       end
-
+      
       if resource.active_for_authentication?
         set_flash_message :notice, :signed_up if is_navigational_format?
-        sign_in(resource_name, resource)
+        sign_up(resource_name, resource)
         respond_with resource, :location => after_sign_up_path_for(resource)
       else
         set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_navigational_format?
@@ -46,7 +49,34 @@ class ConcertoDevise::RegistrationsController < Devise::RegistrationsController
         respond_with resource, :location => after_inactive_sign_up_path_for(resource)
       end
     else
-      render_registration_form(resource)
+      clean_up_passwords resource
+      respond_with resource
+    end
+  end
+
+  # GET /resource/edit
+  def edit
+    render :edit
+  end
+
+  # PUT /resource
+  # We need to use a copy of the resource because we don't want to change
+  # the current user in place.
+  def update
+    self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
+    prev_unconfirmed_email = resource.unconfirmed_email if resource.respond_to?(:unconfirmed_email)
+
+    if resource.update_with_password(account_update_params)
+      if is_navigational_format?
+        flash_key = update_needs_confirmation?(resource, prev_unconfirmed_email) ?
+          :update_needs_confirmation : :updated
+        set_flash_message :notice, flash_key
+      end
+      sign_in resource_name, resource, :bypass => true
+      respond_with resource, :location => after_update_path_for(resource)
+    else
+      clean_up_passwords resource
+      respond_with resource
     end
   end
 
@@ -56,12 +86,13 @@ class ConcertoDevise::RegistrationsController < Devise::RegistrationsController
   def destroy
     if resource.destroy
       Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name)
+      set_flash_message :notice, :destroyed if is_navigational_format?
+      respond_with_navigational(resource){ redirect_to after_sign_out_path_for(resource_name) }
     else
       # This i18n translation comes from Concerto, not Devise
       flash[:notice] = t(:cannot_delete_last_admin)
-    end
-    respond_with_navigational(resource){ redirect_to after_sign_out_path_for(resource_name) }
-  end 
+    end  
+  end
 
   # Decide whether to render the first admin registration page or simply
   # the plain user registration form.
@@ -86,13 +117,65 @@ class ConcertoDevise::RegistrationsController < Devise::RegistrationsController
     end
   end
 
-private
-
-  # Overriden from Devise Controller to implement strong parameters.
-  # In Registrations Controller, used by update, and indirectly by create,
-  # to fetch the posted data about the current resource (user).
-  def resource_params
-    params.require(resource_name).permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password)
+  # GET /resource/cancel
+  # Forces the session data which is usually expired after sign
+  # in to be expired now. This is useful if the user wants to
+  # cancel oauth signing in/up in the middle of the process,
+  # removing all OAuth session data.
+  def cancel
+    expire_session_data_after_sign_in!
+    redirect_to new_registration_path(resource_name)
   end
 
-end 
+  protected
+
+  def update_needs_confirmation?(resource, previous)
+    resource.respond_to?(:pending_reconfirmation?) &&
+      resource.pending_reconfirmation? &&
+      previous != resource.unconfirmed_email
+  end
+
+  # Build a devise resource passing in the session. Useful to move
+  # temporary session data to the newly created user.
+  def build_resource(hash=nil)
+    self.resource = resource_class.new_with_session(hash || {}, session)
+  end
+
+  # Signs in a user on sign up. You can overwrite this method in your own
+  # RegistrationsController.
+  def sign_up(resource_name, resource)
+    sign_in(resource_name, resource)
+  end
+
+  # The path used after sign up. You need to overwrite this method
+  # in your own RegistrationsController.
+  def after_sign_up_path_for(resource)
+    after_sign_in_path_for(resource)
+  end
+
+  # The path used after sign up for inactive accounts. You need to overwrite
+  # this method in your own RegistrationsController.
+  def after_inactive_sign_up_path_for(resource)
+    respond_to?(:root_path) ? root_path : "/"
+  end
+
+  # The default url to be used after updating a resource. You need to overwrite
+  # this method in your own RegistrationsController.
+  def after_update_path_for(resource)
+    signed_in_root_path(resource)
+  end
+
+  # Authenticates the current scope and gets the current resource from the session.
+  def authenticate_scope!
+    send(:"authenticate_#{resource_name}!", :force => true)
+    self.resource = send(:"current_#{resource_name}")
+  end
+
+  def sign_up_params
+    devise_parameter_sanitizer.for(:sign_up)
+  end
+
+  def account_update_params
+    devise_parameter_sanitizer.for(:account_update)
+  end
+end
--- app/controllers/concerto_devise/sessions_controller.rb
+++ app/controllers/concerto_devise/sessions_controller.rb
@@ -1,11 +1,13 @@
 #Overriding the Devise Sessions controller for fun and profit
 class ConcertoDevise::SessionsController < Devise::SessionsController
+  prepend_before_filter :require_no_authentication, :only => [ :new, :create ]
+  prepend_before_filter :allow_params_authentication!, :only => :create
+  prepend_before_filter { request.env["devise.skip_timeout"] = true }
 
   # GET /resource/sign_in
   def new
-    resource = build_resource(nil, :unsafe => true)
+    self.resource = resource_class.new(sign_in_params)
     clean_up_passwords(resource)
-
     # if :no_content_cell param is set to true, do not render the layout or the content cell container within the view
     if params[:no_content_cell]
       respond_with(resource) do |format|
@@ -14,7 +16,44 @@ class ConcertoDevise::SessionsController < Devise::SessionsController
     else
       respond_with (resource)
     end
-
   end
 
+  # POST /resource/sign_in
+  def create
+    self.resource = warden.authenticate!(auth_options)
+    set_flash_message(:notice, :signed_in) if is_navigational_format?
+    sign_in(resource_name, resource)
+    respond_with resource, :location => after_sign_in_path_for(resource)
+  end
+
+  # DELETE /resource/sign_out
+  def destroy
+    redirect_path = after_sign_out_path_for(resource_name)
+    signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
+    set_flash_message :notice, :signed_out if signed_out && is_navigational_format?
+
+    # We actually need to hardcode this as Rails default responder doesn't
+    # support returning empty response on GET request
+    respond_to do |format|
+      format.all { head :no_content }
+      format.any(*navigational_formats) { redirect_to redirect_path }
+    end
+  end
+
+  protected
+
+  def sign_in_params
+    devise_parameter_sanitizer.for(:sign_in)
+  end
+
+  def serialize_options(resource)
+    methods = resource_class.authentication_keys.dup
+    methods = methods.keys if methods.is_a?(Hash)
+    methods << :password if resource.respond_to?(:password)
+    { :methods => methods, :only => [:password] }
+  end
+
+  def auth_options
+    { :scope => resource_name, :recall => "#{controller_path}#new" }
+  end
 end
