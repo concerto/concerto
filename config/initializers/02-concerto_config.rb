4c4641ffd07b83e619f842f59c5b51bb2a2d9e28
Marvin Frederickson <marvin.frederickson@gmail.com>
2013-06-13 23:43:42 -0800

    assign group to config items

--- config/initializers/02-concerto_config.rb
+++ config/initializers/02-concerto_config.rb
@@ -11,32 +11,41 @@ end
 
 if ActiveRecord::Base.connection.table_exists? 'concerto_configs'
   if ConcertoConfig.columns_hash.has_key?("plugin_id")
-    ConcertoConfig.make_concerto_config("default_upload_type", "graphic")
-    ConcertoConfig.make_concerto_config("public_concerto", "true", :value_type => "boolean")
-    ConcertoConfig.make_concerto_config("content_default_start_time", "12:00 am")
-    ConcertoConfig.make_concerto_config("content_default_end_time", "11:59 pm")
-    ConcertoConfig.make_concerto_config("start_date_offset", "0", :value_type => "integer")
-    ConcertoConfig.make_concerto_config("default_content_run_time", "7", :value_type => "integer")
-    ConcertoConfig.make_concerto_config("setup_complete", "false", :value_type => "boolean", :value_default => "true", :hidden => "true")
-    ConcertoConfig.make_concerto_config("allow_registration", "true", :value_type => "boolean")
-    ConcertoConfig.make_concerto_config("allow_user_screen_creation", "false", :value_type => "boolean")
-    ConcertoConfig.make_concerto_config("allow_user_feed_creation", "true", :value_type => "boolean")
-    ConcertoConfig.make_concerto_config("autostart_delayed_job", "true", :value_type => "boolean")
-    ConcertoConfig.make_concerto_config("dynamic_refresh_time", "0", :value_type => "integer", :hidden => "true")
-    ConcertoConfig.make_concerto_config("use_frontend_to_trigger_cron", "false", :value_type => "boolean")
-    ConcertoConfig.make_concerto_config("default_content_duration", "8", :value_type => "integer")
-    ConcertoConfig.make_concerto_config("max_content_duration", "12", :value_type => "integer")
-    ConcertoConfig.make_concerto_config("min_content_duration", "4", :value_type => "integer")   
-    ConcertoConfig.make_concerto_config("mailer_protocol", "sendmail", :value_type => "string")  
-    ConcertoConfig.make_concerto_config("mailer_from", "concerto@localhost", :value_type => "string", :value_default => "concerto@localhost")
-    ConcertoConfig.make_concerto_config("mailer_host", concerto_hostname, :value_type => "string", :value_default => concerto_hostname)
-    ConcertoConfig.make_concerto_config("smtp_address", "", :value_type => "string")
-    ConcertoConfig.make_concerto_config("smtp_port", "587", :value_type => "integer", :value_default => 587)
-    ConcertoConfig.make_concerto_config("smtp_auth_type", "plain", :value_type => "string", :value_default => "plain")
-    ConcertoConfig.make_concerto_config("smtp_username", "", :value_type => "string")
-    ConcertoConfig.make_concerto_config("smtp_password", "", :value_type => "string")  
-    ConcertoConfig.make_concerto_config("system_time_zone", 'Eastern Time (US & Canada)', :value_type => "timezone") 
-    ConcertoConfig.make_concerto_config("config_last_updated", "0", :value_type => "integer", :hidden => "true")
+    # defaults
+    ConcertoConfig.make_concerto_config("default_upload_type", "graphic", :group => 'Content')
+    ConcertoConfig.make_concerto_config("content_default_start_time", "12:00 am", :group => 'Content')
+    ConcertoConfig.make_concerto_config("content_default_end_time", "11:59 pm", :group => 'Content')
+    ConcertoConfig.make_concerto_config("start_date_offset", "0", :value_type => "integer", :group => 'Content')
+    ConcertoConfig.make_concerto_config("default_content_run_time", "7", :value_type => "integer", :group => 'Content')
+    ConcertoConfig.make_concerto_config("default_content_duration", "8", :value_type => "integer", :group => 'Content')
+    ConcertoConfig.make_concerto_config("max_content_duration", "12", :value_type => "integer", :group => 'Content')
+    ConcertoConfig.make_concerto_config("min_content_duration", "4", :value_type => "integer", :group => 'Content')
+
+    # access
+    ConcertoConfig.make_concerto_config("public_concerto", "true", :value_type => "boolean", :group => 'Permissions')
+    ConcertoConfig.make_concerto_config("allow_registration", "true", :value_type => "boolean", :group => 'Permissions')
+    ConcertoConfig.make_concerto_config("allow_user_screen_creation", "false", :value_type => "boolean", :group => 'Permissions')
+    ConcertoConfig.make_concerto_config("allow_user_feed_creation", "true", :value_type => "boolean", :group => 'Permissions')
+
+    # mail
+    ConcertoConfig.make_concerto_config("mailer_protocol", "sendmail", :value_type => "string", :group => 'Mail')
+    ConcertoConfig.make_concerto_config("mailer_from", "concerto@localhost", :value_type => "string", :value_default => "concerto@localhost", :group => 'Mail')
+    ConcertoConfig.make_concerto_config("mailer_host", concerto_hostname, :value_type => "string", :value_default => concerto_hostname, :group => 'Mail')
+    ConcertoConfig.make_concerto_config("smtp_address", "", :value_type => "string", :group => 'Mail')
+    ConcertoConfig.make_concerto_config("smtp_port", "587", :value_type => "integer", :value_default => 587, :group => 'Mail')
+    ConcertoConfig.make_concerto_config("smtp_auth_type", "plain", :value_type => "string", :value_default => "plain", :group => 'Mail')
+    ConcertoConfig.make_concerto_config("smtp_username", "", :value_type => "string", :group => 'Mail')
+    ConcertoConfig.make_concerto_config("smtp_password", "", :value_type => "string", :group => 'Mail')
+
+    # background processing
+    ConcertoConfig.make_concerto_config("dynamic_refresh_time", "0", :value_type => "integer", :hidden => "true", :group => 'Processing')
+    ConcertoConfig.make_concerto_config("autostart_delayed_job", "true", :value_type => "boolean", :group => 'Processing')
+    ConcertoConfig.make_concerto_config("use_frontend_to_trigger_cron", "false", :value_type => "boolean", :group => 'Processing')
+
+    # system
+    ConcertoConfig.make_concerto_config("setup_complete", "false", :value_type => "boolean", :value_default => "true", :hidden => "true", :group => 'System')
+    ConcertoConfig.make_concerto_config("system_time_zone", 'Eastern Time (US & Canada)', :value_type => "timezone", :group => 'System') 
+    ConcertoConfig.make_concerto_config("config_last_updated", "0", :value_type => "integer", :hidden => "true", :group => 'System')
   end
 
   Rails.logger.debug "Completed 02-concerto_config.rb at #{Time.now.to_s}"
