76cd7679cc9cb0294e2c80ce497c6242ed35b423
Brian Michalski <bmichalski@gmail.com>
2013-06-13 19:42:15 -0700

    Bump to 0.3.0.EchoInchworm

--- lib/concerto/version.rb
+++ lib/concerto/version.rb
@@ -1,9 +1,9 @@
 module Concerto
   module VERSION
     MAJOR = 0
-    MINOR = 2
+    MINOR = 3
     TINY = 0
-    PRE = 'deltaocelot'
+    PRE = 'echoinchworm'
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
   end
