7c8bdfb989d3a875e4cea29e08ee2b6e7c6fdeb4
Brian Michalski <bmichalski@gmail.com>
2013-06-18 21:31:54 -0700

    Move "dashboard" aka the config editor to it's own ConcertoConfig controller.
    This has the /settings url.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,24 +1,5 @@
 class DashboardController < ApplicationController
 
-  # GET /dashboard
-  def index
-    authorize! :read, ConcertoConfig   
-    @concerto_configs = ConcertoConfig.where("hidden IS NULL")
-
-    @latest_version = VersionCheck.latest_version()
-  end
-
-  # get a hash of concerto_config keys and values and update them using the ConcertoConfig setter
-  # POST /dashboard/update
-  def update
-    authorize! :update, @concerto_config
-    params[:concerto_config].each  do |k,v|
-      ConcertoConfig.set(k,v) #only set this if the config already exists
-    end
-    flash[:notice] = t(:settings_saved)
-    redirect_to :action => :index
-  end
-
   # GET /dashboard/run_backup
   def run_backup
     require 'concerto-backup'
