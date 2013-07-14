48821c03946ea3858a0c88294c1af1e0e0bf83e0
Zach Rowe <zrowe@bluewatermedia.com>
2013-07-14 18:44:07 -0400

    fixes #534 switch to icon instead of text to indicate group leaders

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -1,20 +1,19 @@
 module GroupsHelper
   def member_list(group=nil)
-    orderedMemberships = group.memberships.approved.order('level DESC').includes(:user).all
-    orderedMemberships.each_with_index do |membership, i|
-      if can? :read, membership.user
-        concat link_to membership.user.name, user_path(membership.user)
-      else
-        concat membership.user.name
+    ordered_memberships = group.memberships.approved.order('level DESC').includes(:user).all
+    member_list = ordered_memberships.map do |membership|
+      member_display = String.new
+      if membership.level == Membership::LEVELS[:leader]
+        member_display = content_tag :i, '', {:class => 'concertocon-user-leader tooltip-basic', 'data-tooltip-tex' => t('.leader')}
       end
-      concat " (leader) " if membership.level == Membership::LEVELS[:leader]
-      if i == 9 && membership != orderedMemberships.last
-        concat ", and "
-        concat link_to "#{orderedMemberships.count - 10} more", group
-        break
-      elsif i != 9 && membership != orderedMemberships.last
-      	concat ", "
+      if can? :read, membership.user
+        member_display += link_to membership.user.name, user_path(membership.user)
+      else
+        member_display += membership.user.name
       end
     end
+    shortened_list = member_list.take(10)
+    shortened_list.push "and #{link_to (ordered_memberships.length - 10).to_s + ' more', group}" if member_list.length > 10
+    shortened_list.join(', ').html_safe
   end
 end
