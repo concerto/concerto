a6f0e09dd4efbd3f2d3b822ed3805b528242f20d
Zach Rowe <zrowe@waterfallfms.com>
2013-08-04 21:25:14 -0400

    add devise confirmable and make it controlled by a config variable. closes #337

---
+++ db/migrate/20130804204252_add_confirmable_to_devise.rb
@@ -0,0 +1,8 @@
+class AddConfirmableToDevise < ActiveRecord::Migration
+  def change
+    add_column :users, :confirmation_token, :string
+    add_column :users, :confirmed_at, :datetime
+    add_column :users, :confirmation_sent_at, :datetime
+    add_column :users, :unconfirmed_email, :string
+  end
+end
