3366322a7605f6f3ec735af1f2239fb54cddb0c5
Marvin Frederickson <marvin.frederickson@gmail.com>
2013-08-06 20:20:28 -0800

    fix new image dimensions to 1,1

--- app/models/template.rb
+++ app/models/template.rb
@@ -78,7 +78,7 @@ class Template < ActiveRecord::Base
     require 'concerto_image_magick'
 
     if self.media.blank?
-      image = ConcertoImageMagick.new_image(640, 480);
+      image = ConcertoImageMagick.new_image(1, 1);
     else
       image = ConcertoImageMagick.load_image(self.media.original.first.file_contents)
     end
