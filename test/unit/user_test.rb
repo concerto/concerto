57a03a2f27325ca24c62bc1521c8da198483d536
Marvin Frederickson <marvin.frederickson@gmail.com>
2013-08-08 15:12:57 -0800

    fixes #616 restrict user deletion if screen owner

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -22,4 +22,14 @@ class UserTest < ActiveSupport::TestCase
     no_groups = @katie.supporting_groups(:feed, feed_levels)
     assert_equal no_groups, []
   end
+
+  test "cannot delete user that owns a screen" do
+    s = screens(:one)
+    screen = Screen.new(s.attributes)
+    screen.owner = @katie
+    screen.save
+    assert_raises(ActiveRecord::DeleteRestrictionError) do
+      @katie.destroy
+    end
+  end
 end
