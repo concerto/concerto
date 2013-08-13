eadd5ebe5c663763904655853f8336f842765672
Marvin Frederickson <marvin.frederickson@gmail.com>
2013-08-13 12:05:48 -0800

    use tags

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,8 +6,10 @@
 #set :user, "www-data"
 #set :group, "www-data"
 # Make sure the deploy_to path points to your deploy location.
-# Run the following two lines at the console
+# Run the following two lines at the console (actually, I couldn't get this part to work, so I added 
+# them to the Gemfile and then ran bundle, and then continued with the cap commands...)
 #$ gem install capistrano
+#$ gem install capistrano-tags
 #$ cap deploy:setup
 #$ cap deploy:check
 # Make sure your database.yml file is correctly set up in your deploy_to location under the shared folder
@@ -16,8 +18,15 @@
 
 set :application, "concerto"
 set :repository,  "https://github.com/concerto/concerto.git"
-set :branch, "master"
 set :asset_env, "#{asset_env} RAILS_RELATIVE_URL_ROOT=/#{application}"
+set :deploy_via, :remote_cache
+
+#set :branch, "master"  # this will deploy the development version
+# this code will get the latest official release
+set :branch do
+  default_tag = `git tag`.split("\n").last
+  default_tag
+end
 
 role :web, "concerto.local"                   # Your HTTP server, Apache/etc
 role :app, "concerto.local"                   # This may be the same as your `Web` server
@@ -38,7 +47,7 @@ default_run_options[:pty] = true # must be true for password prompt from git or 
 
 # if you want to clean up old releases on each deploy uncomment this:
 after "deploy:restart", "deploy:cleanup"
-#after "deploy:update_code", "deploy:migrate"
+after "deploy:update_code", "deploy:migrate"
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
@@ -53,5 +62,10 @@ namespace :deploy do
 end
 
 $LOAD_PATH.unshift File.join(File.dirname(__FILE__), 'deploy')
+
+#set :bundle_flags, "--deployment"   # override, if you want, so --quiet is not specified
+set :bundle_dir, "vendor/bundle"  # this uses separate bundles (as the application specifies, and is very slow)
+
+require "capistrano-tags"   # required because concerto doesnt have a "branch" for each tag
 require "bundler/capistrano"
 require "capistrano_database"
