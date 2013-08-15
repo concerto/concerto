a0ea1a557b6e8165b7b14d46128db91a595d1cca
Marvin Frederickson <marvin.frederickson@gmail.com>
2013-08-14 23:56:51 -0800

    add steps

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,16 +7,31 @@
 #    I ran: `sudo apt-get install capistrano`  and: `sudo gem install capistrano-tags`
 #    a) Make sure the role :web, :app, and :db servers are set to your actual servers (below).
 #    b) Make sure the :deploy_to path points to your actual deploy location.
-#    c) If you need to, make sure the :user and :group are set and uncommented below.
+#    c) If you need to, make sure the :user is set appropriately or comment it out.
 #    d) If you are running the site under a subdirectory instead of at the root of the web server then
 #       uncomment the :asset_env line and make sure the RAILS_RELATIVE_URL_ROOT is set appropriately.
-# 2. Setup the production server(s) for the deploy by running: cap deploy:setup
-#    This will create directories and prompt you for the database password for the concerto user
-#    and set up the database.yml file.  This only ever needs to be run once per server.
+# 2. Do an initial setup on the production server(s) for the deploy by running: 
+#    a) Depending upon your environment, you may not need this-- however, if you are running on the vm image
+#       then you do:
+#         sudo mkdir /var/webapps && chown concerto:concerto /var/webapps && chmod g+w /var/webapps 
+#    b) cap deploy:setup 
+#       This will create directories and prompt you for the database password for the concerto user
+#       and set up the database.yml file.
+#    c) cap deploy:check
 # 3. Run: cap deploy
+# 4. If you're running on the vm image, and you haven't already done so, change the site configuration
+#    to point to the new location where capistrano pushed the files.  Edit your /etc/apache2/sites-enabled/concerto
+#    file to include these two changed lines, replacing their counterparts:
+#      DocumentRoot /var/webapps/concerto/current/public
+#      <Directory /var/webapps/concerto/current/public>
+#    And then reload apache to make the changes take effect: sudo service apache2 reload
+#
+# To update your site with future releases, simply come back here and run: 
+#   cap -S branch="releasename" deploy
+# and you're done! For example, to push out the 1.2.3.foobar release:
+#   cap -S branch="1.2.3.foobar" deploy
 
-#set :user, "deploy"
-#set :group, "deploy"
+set :user, "concerto"
 
 set :application, "concerto"
 set :repository,  "https://github.com/concerto/concerto.git"
