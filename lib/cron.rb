4b474f6e1b89b48d3223c0abdd11d90698660087
Marvin Frederickson <marvin.frederickson@gmail.com>
2013-08-30 08:13:02 -0800

    set sleep timeout to 5s

--- lib/cron.rb
+++ lib/cron.rb
@@ -4,6 +4,11 @@ require './config/boot'
 require './config/environment'
 
 module Clockwork
+
+  Clockwork.configure do |config|
+    config[:sleep_timeout] = 5      # seconds
+  end
+
   every(5.minutes, 'Refresh Dynamic Content') do
     DynamicContent.delay.refresh
   end
