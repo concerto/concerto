bed8866e0e19536f25a95dc96bcae37ed76abc48
August <august@inniogroup.com>
2013-10-11 14:18:28 -0400

    Set session store to use the cache store per #737

--- config/initializers/08-session_store.rb
+++ config/initializers/08-session_store.rb
@@ -1,6 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-Concerto::Application.config.session_store :cookie_store, :key => '_concerto_session'
+Concerto::Application.config.session_store :cache_store
+
+#Concerto::Application.config.session_store :cookie_store, :key => '_concerto_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
