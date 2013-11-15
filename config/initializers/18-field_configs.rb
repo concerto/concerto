f2cb61b2fdfb2f7a2c26855b334c853b367dd01f
August <august@inniogroup.com>
2013-11-14 21:17:58 -0500

    On #752, updating the hash structure to be dict-style for easy name lookup and insert

--- config/initializers/18-field_configs.rb
+++ config/initializers/18-field_configs.rb
@@ -2,21 +2,14 @@
 # Each entry has:
 #  the fieldconfig name (a string)
 #  the type of data it represents (boolean, text, select, checkbox) 
-#  and its possible values (comma seperated strings)
+#  and its possible values (array of values)
 
 #start an empty array
 field_configs_array = []
 
-#set up each hash and push it onto the end of the array
 
-#Transition
-hash = { :name => :transition, :type => :select, :values => "fade,slide,replace" }
-field_configs_array.push(hash)
-
-#Format
-hash = { :name => :time_format, :type => :text, :values => "" }
-field_configs_array.push(hash)
- 
-#shove all of this into the Application object for global access
-Concerto::Application.config.field_configs = field_configs_array
-
+#put the dictionary hash into the Application object for global access
+Concerto::Application.config.field_configs = {
+  :transition => {:type => :select, :values => ['fade','slide','replace']}, 
+  :time_format => {:type => "string"}
+}
