3ed9ecd49f347e36e9859a6f9d97e69da57925b8
Marvin Frederickson <marvin.frederickson@gmail.com>
2013-12-30 14:05:56 -0900

    resequence migration so runs

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131221173425) do
+ActiveRecord::Schema.define(:version => 20131230202517) do
 
   create_table "activities", :force => true do |t|
     t.integer  "trackable_id"
