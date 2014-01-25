d156aac82a9730950066376c568d6241249cea82
Zach Rowe <zach@alum.rpi.edu>
2014-01-24 22:30:37 -0500

    read configs into test suite

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,6 +5,14 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 include Devise::TestHelpers
 
+#read in db configs, per http://docs.travis-ci.com/user/database-setup/#A-Ruby-specific-Approach
+configs = YAML.load_file('test/database.yml')
+ActiveRecord::Base.configurations = configs
+
+db_name = ENV['DB'] || 'sqlite'
+ActiveRecord::Base.establish_connection(db_name)
+ActiveRecord::Base.default_timezone = :utc
+
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
   #
