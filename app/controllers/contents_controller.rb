17c30ee292d6de155fd00183dcdc8be2225b1a86
Mike <mikldt@gmail.com>
2014-01-29 22:32:08 -0500

    Fix broken response to contents#update
    Previously resulted in a 406 error.

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -152,33 +152,31 @@ class ContentsController < ApplicationController
   # PUT /contents/1.xml
   def update
     @content = Content.find(params[:id])
+    @user = User.find(@content.user_id)
     auth!
 
     @feed_ids = feed_ids
 
-    respond_to do |format|
-      if @content.update_attributes(content_update_params)
-        process_notification(@content, {}, :action => 'update', :owner => current_user)
-        submissions = @content.submissions
-        submissions.each do |submission|
-          if @feed_ids.include? submission.feed_id
-            submission.update_attributes(:moderation_flag => nil)
-          else
-            submission.mark_for_destruction
-          end
+    if @content.update_attributes(content_update_params)
+      process_notification(@content, {}, :action => 'update', :owner => current_user)
+      submissions = @content.submissions
+      submissions.each do |submission|
+        if @feed_ids.include? submission.feed_id
+          submission.update_attributes(:moderation_flag => nil)
+        else
+          submission.mark_for_destruction
         end
-        submitted_feeds = submissions.map { |s| s.feed_id }
-        @feed_ids.reject! { |id| submitted_feeds.include? id }
-        create_submissions
-        if @content.save
-          flash[:notice] = t(:content_updated)
-        end
-        respond_with(@content)
-      else
-        @feeds = submittable_feeds
-        format.html { render :action => "edit" }
-        format.xml { render :xml => @content.errors, :status => :unprocessable_entity }
       end
+      submitted_feeds = submissions.map { |s| s.feed_id }
+      @feed_ids.reject! { |id| submitted_feeds.include? id }
+      create_submissions
+      if @content.save
+        flash[:notice] = t(:content_updated)
+      end
+      respond_with(@content)
+    else
+      @feeds = submittable_feeds
+      respond_with(@content)
     end
   end
 
