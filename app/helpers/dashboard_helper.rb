e52c75fdc92dcc4cb3727ac1c979da04354a04ae
Marvin Frederickson <marvin.frederickson@gmail.com>
2014-01-30 22:55:18 -0900

    use attribute name for name of item

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -29,16 +29,17 @@ module DashboardHelper
   # @param [PublicActivity::Activity] activity Activity instance from which to obtain the item.
   #   If the item no longer exists, the item_name from the parameters will be used, if it exists.
   #   For example, if the trackable activity is a screen then the item_name would be :screen_name.
+  # @param [String] attr_name The attribute that represents the name of the instance of the item.
   # @return [String] The possibly linkable trackable item of the activity.
-  def get_activity_item(activity)
+  def get_activity_item(activity, attr_name = 'name')
     if activity.trackable
       if can? :read, activity.trackable
-        link_to(activity.trackable.name, activity.trackable)
+        link_to(activity.trackable.send(attr_name), activity.trackable)
       else
-        activity.trackable.name
+        activity.trackable.send(attr_name)
       end
     else
-      trackable_name_sym = (activity.trackable_type.downcase + '_name').to_sym
+      trackable_name_sym = (activity.trackable_type.underscore.downcase + '_' + attr_name).to_sym
       if activity.parameters.include?(trackable_name_sym)
         activity.parameters[trackable_name_sym]
       else
@@ -51,12 +52,13 @@ module DashboardHelper
   #
   # @param [PublicActivity::Activity] activity Activity instance.
   #   This requires a translation key of "#{action}_the_model" and uses the trackable type's model's human name.
+  # @param [String] attr_name The attribute that represents the name of the instance of the item.
   # @return [String] The view contents.
-  def generate_activity_view(activity)
+  def generate_activity_view(activity, attr_name = 'name')
     action = activity.key.split(".").last
     results = get_activity_owner(activity) + 
       " " + t((action + "_the_model").to_sym, :model => activity.trackable_type.classify.safe_constantize.model_name.human.downcase) +
-      " " + get_activity_item(activity)
+      " " + get_activity_item(activity, attr_name)
     results.html_safe
   end
 end
