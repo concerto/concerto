ac2073175604dcc424b83b4563edb40ab009c6de
Marvin Frederickson <marvin.frederickson@gmail.com>
2014-03-03 17:13:45 -0900

    add tests for 100% coverage for user model

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -23,13 +23,40 @@ class UserTest < ActiveSupport::TestCase
     assert_equal [], no_groups
   end
 
+  test "in_group?" do
+    assert @karen.in_group?(groups(:wtg))
+    assert !@karen.in_group?(groups(:rpitv))
+  end
+
   test "cannot delete user that owns a screen" do
-    s = screens(:one)
-    screen = Screen.new(s.attributes)
-    screen.owner = @katie
-    screen.save
+    assert !@katie.is_deletable?
+    assert @karen.is_deletable?
+
     assert_raises(ActiveRecord::DeleteRestrictionError) do
       @katie.destroy
     end
+  end
+
+  test "cannot delete last admin" do
+    assert !users(:admin).destroy
+    assert users(:karen).destroy
+  end
+
+  test "owned feeds" do
+    assert @katie.owned_feeds.include?(feeds(:service))
+    assert !@karen.owned_feeds.include?(feeds(:service))
+  end
+
+  test "auto confirm" do
+    ConcertoConfig.set :confirmable, false
+    assert_equal ConcertoConfig[:confirmable], false
+    bob = User.create({
+      :first_name => 'Bob',
+      :last_name => 'Rowe',
+      :email => 'bob@rowebot.com',
+      :password => 'bubkisbubkis'
+    })
+
+    assert_equal Date.new(1824, 11, 5), bob.confirmed_at.to_date
   end
 end
