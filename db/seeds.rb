acd08bf5c48d7de90760a77d716fce130fc49df8
Zach Rowe <zrowe007@gmail.com>
2014-04-27 15:32:08 -0400

    cherry-pick db/seeds from rails 4 branch, fix rails 3.2 compatability issues to close #914
    Conflicts:
    	db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,24 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 
-#It's important to use find_or_create_by_{attribute} so as to prevent duplicate creation of records
+#It's important to use where{attribute}.first_or_create so as to prevent duplicate creation of records
 
 # Populate the 3 major 'kinds' of content we know as of now.
 # There is discussion to move this to a static array / config elsewhere,
-# but I don't have a solid grasp on the system-wide reprecussions of that
+# but I don't have a solid grasp on the system-wide repercussions of that
 # change at the moment.
 # Note: This is replicated in config/initializers/17-required_data.rb because an instance must have kinds.
 ["Graphics", "Ticker", "Text", "Dynamic"].each do |kind|
-  Kind.find_or_create_by_name kind
+  Kind.where(:name => kind).first_or_create
 end
 
 #Default plugins
-ConcertoPlugin.find_or_create_by_gem_name({:gem_name => "concerto_weather", :enabled => true, :source => "rubygems"})
-ConcertoPlugin.find_or_create_by_gem_name({:gem_name => "concerto_remote_video", :enabled => true, :source => "rubygems"})
-ConcertoPlugin.find_or_create_by_gem_name({:gem_name => "concerto_simple_rss", :enabled => true, :source => "rubygems"})
-ConcertoPlugin.find_or_create_by_gem_name({:gem_name => "concerto_iframe", :enabled => true, :source => "rubygems"})
-ConcertoPlugin.find_or_create_by_gem_name({:gem_name => "concerto_calendar", :enabled => true, :source => "rubygems"})
-ConcertoPlugin.find_or_create_by_gem_name({:gem_name => "concerto-hardware", :enabled => true, :source => "rubygems"})
+ConcertoPlugin.where(:gem_name => "concerto_weather").first_or_create(:enabled => true, :source => "rubygems")
+ConcertoPlugin.where(:gem_name => "concerto_remote_video").first_or_create(:enabled => true, :source => "rubygems")
+ConcertoPlugin.where(:gem_name => "concerto_simple_rss").first_or_create(:enabled => true, :source => "rubygems")
+ConcertoPlugin.where(:gem_name => "concerto_iframe").first_or_create(:enabled => true, :source => "rubygems")
+ConcertoPlugin.where(:gem_name => "concerto_calendar").first_or_create(:enabled => true, :source => "rubygems")
+ConcertoPlugin.where(:gem_name => "concerto-hardware").first_or_create(:enabled => false, :source => "rubygems")
 
 # Establish the 4 major display areas a template usually has.
 # In my quick sample, this code will make 68% of the Concerto 1 fields match
@@ -27,16 +27,15 @@ ConcertoPlugin.find_or_create_by_gem_name({:gem_name => "concerto-hardware", :en
 
 # Note: This is replicated in config/initializers/17-required_data.rb because an instance must have fields.
 Kind.all.each do |kind|
-  field = Field.find_or_create_by_name({:name => kind.name, :kind => Kind.where(:name => kind.name).first})
-end
-# The time is just a special text field.
-time_field = Field.find_or_create_by_name({:name => 'Time', :kind => Kind.where(:name => 'Text').first})
-if !FieldConfig.default.where(:field_id => time_field.id, :key => 'transition').exists?
-  FieldConfig.create(:field_id => time_field.id, :screen_id => nil, :key => 'transition', :value => 'replace')
+  field = Field.where(:name => kind.name).first_or_create(:kind => Kind.where(:name => kind.name).first)
 end
 
+# The time is just a special text field.
+time_field = Field.where(:name => 'Time').first_or_create(:kind => Kind.where(:name => 'Text').first)
+FieldConfig.default.where(:field_id => time_field.id, :key => 'transition').first_or_create(:value => 'replace')
+
 #Create an initial group
-Group.find_or_create_by_name(:name => "Concerto Admins")
+Group.where(:name => "Concerto Admins").first_or_create
 
 #Determine installed content types for enabling them in the inital feed
 #This is not the ideal way but unfortunately they're not registered yet at this point
@@ -53,15 +52,15 @@ if Gem.loaded_specs.has_key? "concerto_weather"
 end
 
 #Create an initial feed
-Feed.find_or_create_by_name(:name => "Concerto", 
-  :description => "Initial Concerto Feed", 
-  :group_id => Group.first.id, 
-  :is_viewable => 1, 
-  :is_submittable => 1, 
-  :content_types => installed_content_types)
+Feed.where(:name => "Concerto").first_or_create(
+    :description => "Initial Concerto Feed",
+    :group_id => Group.first.id,
+    :is_viewable => 1,
+    :is_submittable => 1,
+    :content_types => installed_content_types)
 
 #Create an initial template
-@template = Template.find_or_create_by_name(:name => "Default Template", :author => "Concerto")
+@template = Template.where(:name => "Default Template").first_or_create(:author => "Concerto")
 
 #Taking care to make this file upload idempotent
 if Media.where(:file_name => "BlueSwooshNeo_16x9.jpg").empty?
@@ -71,17 +70,17 @@ end
 
 #Associate each field with a position in the template
 concerto_template = Template.where(:name => "Default Template").first.id
-Position.find_or_create_by_field_id_and_template_id(Field.where(:name => "Graphics").first.id,concerto_template, :top => ".026", :left => ".025", :bottom => ".796", :right => ".592", :style => "border:solid 2px #ccc;")
-Position.find_or_create_by_field_id_and_template_id(Field.where(:name => "Ticker").first.id,concerto_template, :top => ".885", :left => ".221", :bottom => ".985", :right => ".975", :style => "color:#FFF; font-family:Frobisher, Arial, sans-serif; font-weight:bold !important;")
-Position.find_or_create_by_field_id_and_template_id(Field.where(:name => "Text").first.id,concerto_template, :top => ".015", :left => ".68", :bottom => ".811", :right => ".98", :style =>"color:#FFF; font-family:Frobisher, Arial, sans-serif;")
-Position.find_or_create_by_field_id_and_template_id(Field.where(:name => "Time").first.id,concerto_template, :top => ".885", :left => ".024", :bottom => ".974", :right => ".18", :style => "color:#ccc; font-family:Frobisher, Arial, sans-serif; font-weight:bold !important; letter-spacing:.12em !important;")
+Position.where(:field_id => Field.where(:name => "Graphics").first.id, :template_id => concerto_template).first_or_create(:top => ".026", :left => ".025", :bottom => ".796", :right => ".592", :style => "border:solid 2px #ccc;")
+Position.where(:field_id => Field.where(:name => "Ticker").first.id, :template_id => concerto_template).first_or_create(:top => ".885", :left => ".221", :bottom => ".985", :right => ".975", :style => "color:#FFF; font-family:Frobisher, Arial, sans-serif; font-weight:bold !important;")
+Position.where(:field_id => Field.where(:name => "Text").first.id, :template_id => concerto_template).first_or_create(:top => ".015", :left => ".68", :bottom => ".811", :right => ".98", :style =>"color:#FFF; font-family:Frobisher, Arial, sans-serif;")
+Position.where(:field_id => Field.where(:name => "Time").first.id, :template_id => concerto_template).first_or_create(:top => ".885", :left => ".024", :bottom => ".974", :right => ".18", :style => "color:#ccc; font-family:Frobisher, Arial, sans-serif; font-weight:bold !important; letter-spacing:.12em !important;")
 
 #Create a sample Full-Screen
-Screen.find_or_create_by_name(:name => "Sample Screen", :location => "Cafe", :is_public => true, :owner_id => Group.first.id, :owner_type => "Group", :template_id => concerto_template, :width => 1024, :height => 768)
+Screen.where(:name => "Sample Screen").first_or_create(:location => "Cafe", :is_public => true, :owner_id => Group.first.id, :owner_type => "Group", :template_id => concerto_template, :width => 1024, :height => 768)
 
 #Create initial subscriptions for the sample Screen
 feed_id = Feed.first.id
 screen_id= Screen.first.id
 Field.where('name NOT IN (?)', ['Dynamic', 'Time']).each do |f|
-  Subscription.where(:feed_id => feed_id, :field_id => f.id, :screen_id => screen_id, :weight => 1).first_or_create
+  Subscription.where(:feed_id => feed_id, :field_id => f.id, :screen_id => screen_id).first_or_create(:weight => 1)
 end
