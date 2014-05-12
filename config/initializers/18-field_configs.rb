5c57b7d86544cb26240ae6149d83028ce67188f9
August <august@inniogroup.com>
2014-05-12 18:55:02 -0400

    Added field config for #904

--- config/initializers/18-field_configs.rb
+++ config/initializers/18-field_configs.rb
@@ -7,4 +7,5 @@
 Concerto::Application.config.field_configs = {
   :transition => {:type => :select, :values => ['fade','slide','replace']},
   :time_format => {:type => :string}
+  :autosize_text => {:type => :boolean}
 }
