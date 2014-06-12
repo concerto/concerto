34bb76ec210cbb0d799a9c0029b85d2f04814c29
Marvin Frederickson <marvin.frederickson@gmail.com>
2014-06-12 00:20:33 -0800

    support scrolling marquee in ticker field

--- public/frontend_js/contents/ticker.js
+++ public/frontend_js/contents/ticker.js
@@ -24,12 +24,25 @@ concerto.frontend.Content.Ticker = function(data) {
   var disable_text_autosize = !!+(data['field']['config'] ?
                                data['field']['config']['disable_text_autosize'] : 0);
 
+  var scrolling = !!+(data['field']['config'] ?
+                               data['field']['config']['scrolling'] : 0);
   /**
    * Should the font size be automatically adjusted to optimize
    * display within the field?
    * @type {boolean}
    */
   this.autosize_font = !disable_text_autosize;
+
+  /**
+   * Should ticker be displayed as a scrolling marquee
+   * @type {boolean}
+   * @private
+   */
+  this.scrolling_ = scrolling;
+
+  if (this.scrolling_) {
+    goog.dom.classes.add(this.div_, 'marquee');
+  }
 
   /**
    * The text.
@@ -64,12 +77,60 @@ concerto.frontend.ContentTypeRegistry['Ticker'] =
  * @private
  */
 concerto.frontend.Content.Ticker.prototype.load_ = function() {
-  // plain text ticker
-  // goog.dom.setTextContent(this.div_, this.text);
-
-  // html ticker, wrapped in single node
   goog.dom.removeChildren(this.div_);
-  var frag = goog.dom.htmlToDocumentFragment('<div>' + this.text + '</div>');
+  var frag = null;
+  if (this.scrolling_) {
+    frag = goog.dom.htmlToDocumentFragment(
+/* TODO: need to get all the stuff to scroll and add it here as spans */
+      '<div class="marquee-bundle">' + 
+        '<span>' + this.text + '</span>' +
+        '<span>' + this.text + '</span>' +
+        '<span>' + this.text + '</span>' +
+        '<span>' + this.text + '</span>' +
+        '<span>' + this.text + '</span>' +
+      '</div>' +
+
+      '<div class="marquee-bundle">' + 
+        '<span>' + this.text + '</span>' +
+        '<span>' + this.text + '</span>' +
+        '<span>' + this.text + '</span>' +
+        '<span>' + this.text + '</span>' +
+        '<span>' + this.text + '</span>' +
+      '</div>'
+      );
+  } else {
+    frag = goog.dom.htmlToDocumentFragment('<div>' + this.text + '</div>');
+  }
   goog.dom.appendChild(this.div_, frag);
   this.finishLoad();
+
+  this.setScrollDuration_();
 };
+
+/**
+ * Set the scroll animation duration based on length of text to scroll.
+ * @private
+ */
+concerto.frontend.Content.Ticker.prototype.setScrollDuration_ = function() {
+  /* TODO exclude html markup in count */
+  /* TODO field config the speed */
+  var dur = Math.floor(goog.dom.getNodeTextLength(this.div_)/15);
+
+  goog.style.setStyle(this.div_, 'webkitAnimationDuration', dur + 's');
+  goog.style.setStyle(this.div_, 'mozAnimationDuration', dur + 's');
+  goog.style.setStyle(this.div_, 'msAnimationDuration', dur + 's');
+  goog.style.setStyle(this.div_, 'animationDuration', dur + 's');
+};
+
+/**
+ * Extend the default style application.
+ * We need to take into account relative positioning for scrolling
+ * @extends {concerto.frontend.Content.applyStyles}
+ */
+concerto.frontend.Content.Ticker.prototype.applyStyles = function(styles) {
+  concerto.frontend.Content.Ticker.superClass_.applyStyles.call(this, styles);
+  if (this.scrolling_) {
+    goog.style.setStyle(this.div_, 'position', 'relative');
+  }
+};
+
