b79ab12e22b52520c0a586ed2b64bd014c014c8a
Pascal Brogle <pascal.brogle@simplysoft.ch>
2014-06-18 15:55:24 +0200

    fix engine detection by only using comparing base name

--- app/models/concerto_plugin.rb
+++ app/models/concerto_plugin.rb
@@ -196,12 +196,12 @@ private
     result = nil
     # We already know the name of the gem from user input
     if Gem.loaded_specs.has_key? gem_name
-      # Let's get the gem's full name
-      name = Gem.loaded_specs[gem_name].full_name
+      # Let's get the gem's basename in the filesystem
+      gem_basename = Pathname(Gem.loaded_specs[gem_name].full_gem_path).basename
       # Then match the name we've got to the name of an engine -
       #    which should have its Module Name (aka paydirt)
       Rails::Application::Railties.engines.each do |engine|
-        if engine.class.root.basename.to_s == name
+        if engine.class.root.basename == gem_basename
           # Get the class name from the engine hash
           result = engine.class
           break
