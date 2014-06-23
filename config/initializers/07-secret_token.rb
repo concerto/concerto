1de727d61ac081829c68ad2c96bd1099d14f12f9
August <august@inniogroup.com>
2014-06-22 22:14:02 -0400

    Auto generate secret token per #1013

--- config/initializers/07-secret_token.rb
+++ config/initializers/07-secret_token.rb
@@ -1,7 +1,7 @@
-# Be sure to restart your server when you modify this file.
+# Secret key for verifying the integrity of signed cookies.
 
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Concerto::Application.config.secret_token = '55fed69ff18aa21f4ab5d3482e368c9733595fb4428e116f31ab45f4eab8e712ea87ac4daeec032d2a6c2301a8ca376f070927e65e2c609890c2e0a314948d3a'
+if ConcertoConfig[:secret_token] == ""
+  ConcertoConfig.set("secret_token", SecureRandom.hex(64))
+end
+
+Concerto::Application.config.secret_token = ConcertoConfig[:secret_token]
