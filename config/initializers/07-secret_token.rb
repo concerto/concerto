cadcd8a67f325abcebebd4c30f9b6ca3d9d317e6
August <august@inniogroup.com>
2014-06-22 22:25:38 -0400

    Dealing with the old problem of Travis not having DB tables ready during initialization

--- config/initializers/07-secret_token.rb
+++ config/initializers/07-secret_token.rb
@@ -1,7 +1,7 @@
 # Secret key for verifying the integrity of signed cookies.
-
-if ConcertoConfig[:secret_token] == ""
-  ConcertoConfig.set("secret_token", SecureRandom.hex(64))
+if ActiveRecord::Base.connection.table_exists? 'concerto_configs'
+  if ConcertoConfig[:secret_token] == ""
+    ConcertoConfig.set("secret_token", SecureRandom.hex(64))
+  end
+  Concerto::Application.config.secret_token = ConcertoConfig[:secret_token]
 end
-
-Concerto::Application.config.secret_token = ConcertoConfig[:secret_token]
