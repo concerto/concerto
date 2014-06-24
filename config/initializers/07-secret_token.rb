8edd5298bb8213936d64b6d31349c7f7e5a0cb54
Pascal Brogle <pascal.brogle@simplysoft.ch>
2014-06-24 18:51:25 +0200

    allow to provide secret token via environment variable
    fixes #1013

--- config/initializers/07-secret_token.rb
+++ config/initializers/07-secret_token.rb
@@ -1,7 +1,20 @@
-# Secret key for verifying the integrity of signed cookies.
-if ActiveRecord::Base.connection.table_exists? 'concerto_configs'
-  if ConcertoConfig[:secret_token] == ""
-    ConcertoConfig.set("secret_token", SecureRandom.hex(64))
+Rails.logger.debug "Starting 07-secret_token.rb at #{Time.now.to_s}"
+
+secret_token = ENV['SECRET_TOKEN']
+
+if secret_token.blank? && ActiveRecord::Base.connection.table_exists?('concerto_configs')
+  # Try go get secret key from concerto config or auto-generate it
+  secret_token = ConcertoConfig[:secret_token]
+
+  if secret_token.blank?
+    require 'securerandom'
+    secret_token = SecureRandom.hex(64)
+    ConcertoConfig.set('secret_token', secret_token)
+    Rails.logger.debug 'Auto-generated secret token'
   end
-  Concerto::Application.config.secret_token = ConcertoConfig[:secret_token]
 end
+
+# Secret key for verifying the integrity of signed cookies.
+Concerto::Application.config.secret_token = secret_token
+
+Rails.logger.debug "Completed 07-secret_token.rb at #{Time.now.to_s}"
