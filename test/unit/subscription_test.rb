9e61e9458d849f344b6d1024d35aede411bf15af
Brian Michalski <bmichalski@gmail.com>
2014-08-02 17:35:13 -0400

    Add validations to enforce subscriptions belonging to a field and screen.
    A better fix to prevent getting into the state of #1091.

--- test/unit/subscription_test.rb
+++ test/unit/subscription_test.rb
@@ -4,4 +4,47 @@ class SubscriptionTest < ActiveSupport::TestCase
   test "weight names" do
     assert_equal "very rarely", subscriptions(:one).weight_name
   end
+
+  test "screen cannot unassociated" do
+    subscription = Subscription.new(:field_id => fields(:two).id,
+                                    :feed_id => feeds(:service).id,
+                                    :weight => Subscription::WEIGHTS[:rarely])
+    assert subscription.invalid?, "Subscription screen is blank"
+    subscription.screen_id = 0
+    assert subscription.invalid?, "Subscription screen is unassociated"
+    subscription.screen = screens(:two)
+    assert subscription.valid?, subscription.errors.to_yaml
+  end
+
+  test "field cannot unassociated" do
+    subscription = Subscription.new(:screen_id => screens(:two).id,
+                                    :feed_id => feeds(:service).id,
+                                    :weight => Subscription::WEIGHTS[:rarely])
+    assert subscription.invalid?, "Subscription field is blank"
+    subscription.field_id = 0
+    assert subscription.invalid?, "Subscription field is unassociated"
+    subscription.field = fields(:two)
+    assert subscription.valid?, subscription.errors.to_yaml
+  end
+
+  test "feed cannot unassociated" do
+    subscription = Subscription.new(:screen_id => screens(:two).id,
+                                    :field_id => fields(:two).id,
+                                    :weight => Subscription::WEIGHTS[:rarely])
+    assert subscription.invalid?, "Subscription feed is blank"
+    subscription.feed_id = 0
+    assert subscription.invalid?, "Subscription feed is unassociated"
+    subscription.feed = feeds(:service)
+    assert subscription.valid?, subscription.errors.to_yaml
+  end
+
+  test "feed cannot be duplicated" do
+    subscription = Subscription.new(:screen_id => screens(:one).id,
+                                    :field_id => fields(:one).id,
+                                    :feed_id => feeds(:service).id,
+                                    :weight => Subscription::WEIGHTS[:rarely])
+    assert subscription.invalid?, "Duplicate subscription"
+    subscription.feed = feeds(:boring_announcements)
+    assert subscription.valid?, subscription.errors.to_yaml
+  end
 end
