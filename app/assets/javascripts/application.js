63e7d0a9b872ccaa19b8429a5486e694054c7277
Pascal Brogle <pascal.brogle@simplysoft.ch>
2015-01-10 00:06:37 +0100

    fix boostrap rails cofirm load order

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,12 +14,12 @@
 //= require jquery_ujs
 //= require html5_shiv/html5
 //= require i18n/translations
-//= require twitter/bootstrap/rails/confirm
 //= require bootstrap-datepicker/core
 //  NOTE: right now only the English (default) is provided for the bootstrap-datepicker; if all internationalizations should be included, just remove the '/core' part of the line above and make it just 'require bootstrap-datepicker'
 //= require bootstrap-alert.js
 //= require bootstrap-modal.js
 //= require bootstrap-tab.js
+//= require twitter/bootstrap/rails/confirm
 //= require jquery.timepicker.js
 //= require jquery.datepair.js
 //= require jquery.iframe-transport
