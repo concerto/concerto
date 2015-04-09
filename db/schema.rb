16fc86264dc712a205e20ac82df2d6b192d9200d
August <august@inniogroup.com>
2015-04-08 20:16:14 -0400

    Add time zone for screens per #498; timezone is still nil in the frontend JS

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140804171558) do
+ActiveRecord::Schema.define(version: 20150408235435) do
 
   create_table "activities", force: true do |t|
     t.integer  "trackable_id"
@@ -197,11 +197,9 @@ ActiveRecord::Schema.define(version: 20140804171558) do
     t.integer  "template_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "width"
-    t.integer  "height"
     t.datetime "frontend_updated_at"
     t.string   "authentication_token"
-    t.string   "locale"
+    t.string   "time_zone"
   end
 
   create_table "submissions", force: true do |t|
