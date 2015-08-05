ab5719127262f31a3054140ced832f1915bde5ec
August <august@inniogroup.com>
2015-08-05 11:40:21 -0400

    When a auth plugin is installed, user deletion must delete the identity too, or things will break

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,22 @@
 class User < ActiveRecord::Base
   include ActiveModel::ForbiddenAttributesProtection
-  #Newsfeed
+  belongs_to_junction_instance
+
   include PublicActivity::Common if defined? PublicActivity::Common
 
+  #If the concerto identity plugin is installed, allow the deletion of identity records with users
+  if Object.const_defined?('ConcertoIdentity')
+    has_one :concerto_identity, :class_name => "ConcertoIdentity::Identity", :dependent => :destroy
+  end
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable,
   # :lockable, :timeoutable and :omniauthable, :trackable
-  modules = [:database_authenticatable, :recoverable, :registerable, :rememberable, :validatable]
+  modules = [:database_authenticatable, :recoverable, :registerable, :rememberable]
   if ActiveRecord::Base.connection.table_exists? 'concerto_configs'
     modules << :confirmable if ConcertoConfig[:confirmable]
   end
   devise *modules
-         
+
   before_destroy :dont_delete_last_admin
   before_create :auto_confirm
 
@@ -26,8 +31,15 @@ class User < ActiveRecord::Base
   has_many :leading_groups, -> { where "memberships.level" => Membership::LEVELS[:leader]}, through: :memberships, source: :group
 
   # Validations
-  validates :first_name, presence: true
-  
+  validates :first_name, :presence => true
+
+  # Devise Validations
+  # We do not inherit these from devise, because it has
+  # unintelligent email uniqueness checks.
+  validates_presence_of   :email
+  validates_uniqueness_of :email, :scope => :instance_id
+  validates_format_of     :email, :with => Devise.email_regexp, :allow_blank => true, :if => :email_changed?
+
   scope :admin, -> { where is_admin: true }
 
   def auto_confirm
@@ -35,12 +47,12 @@ class User < ActiveRecord::Base
     # this new user will not be locked out
     self.confirmed_at = Time.zone.local(1824, 11, 5) if !ConcertoConfig[:confirmable]
   end
-  
+
   #a user who isn't the last admin and owns no screens is deletable
   def is_deletable?
     self.screens.size == 0 && !is_last_admin?
   end
-  
+
   #when we only have one user in the system who is the admin
   def is_last_admin?
     User.admin.count == 1 && self.is_admin?
@@ -69,7 +81,7 @@ class User < ActiveRecord::Base
   def owned_feeds
     leading_groups.collect{|g| g.feeds}.flatten
   end
-  
+
   # Return an array of all the groups a user has a certain regular permission for.
   def supporting_groups(type, permissions)
     supporting_groups =  groups.select{|g| g.user_has_permissions?(self, :regular, type, permissions)}
