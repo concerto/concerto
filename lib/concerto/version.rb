e94b5acb60f57a5676803e161399dfd04b83d8df
August <august@inniogroup.com>
2015-10-30 02:24:42 -0400

    Fixed #1271 by catching API error and fixed version string method to not end with a dot.

--- lib/concerto/version.rb
+++ lib/concerto/version.rb
@@ -4,9 +4,9 @@ module Concerto
     MINOR = 3
     TINY = 0
     PRE = ''
-
-    STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
     
+    STRING = [MAJOR, MINOR, TINY, PRE].reduce('') { |str,s| str + s.to_s + '.' }[0..-3]
+
     def self.dynamic
       Rails.cache.fetch('VERSION::dynamic') do
         `git describe --always --tags`.strip rescue ""
