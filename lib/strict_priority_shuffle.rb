43bc91566d9e3d4307d6ed5ebd42a58b1079ecb0
Diego Garcia del Rio <garci66@gmail.com>
2016-12-13 19:09:14 -0600

    Fix an extra not and simplify comparison

--- lib/strict_priority_shuffle.rb
+++ lib/strict_priority_shuffle.rb
@@ -14,6 +14,6 @@ class StrictPriorityShuffle < BaseShuffle
 
   def prioritised_content
     highest_prio=@subscriptions.max_by{|s| s.weight}
-    @subscriptions.collect{|s| s.contents * (!s.weight == highest_prio ? 0 : 1)}.flatten.shuffle!
+    @subscriptions.collect{|s| s.contents * (s.weight == highest_prio)}.flatten.shuffle!
   end
 end
