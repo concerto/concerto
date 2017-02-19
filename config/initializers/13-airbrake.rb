acdbcf1e21b6cb4edc0d6569ec5bd55141a0aa99
Marvin Frederickson <marvin.frederickson@gmail.com>
2017-02-19 14:05:22 -0900

    project_id nonzero and key required even if not notifying

--- config/initializers/13-airbrake.rb
+++ config/initializers/13-airbrake.rb
@@ -1,21 +1,22 @@
 Rails.logger.debug "Starting #{File.basename(__FILE__)} at #{Time.now.to_s}"
 
 require 'yaml'
-concerto_base_config = YAML.load_file("./config/concerto.yml")
+concerto_base_config = YAML.load_file('./config/concerto.yml')
 
 if ActiveRecord::Base.connection.table_exists? 'concerto_configs'
-  ConcertoConfig.make_concerto_config("send_errors", "#{concerto_base_config['airbrake_enabled_initially'].to_s}", value_type: "boolean", category: "System")
+  ConcertoConfig.make_concerto_config('send_errors',
+    "#{concerto_base_config['airbrake_enabled_initially'].to_s}",
+    value_type: 'boolean', category: 'System')
 
   if defined?(Airbrake)
     Airbrake.configure do |config|
-      config.project_key = '34e36775df3e89293c59efeba36f6c8f' if ConcertoConfig[:send_errors]
-      config.project_key ||= ''
-      config.project_id = 0
+      config.project_key = '34e36775df3e89293c59efeba36f6c8f'
+      config.project_id = 1
       config.host = 'http://errors.concerto-signage.org:80'
       config.environment = Concerto::VERSION::STRING
 
       # Uncomment the following to start reporting development mode errors.
-      #config.ignore_environments = []
+      # config.ignore_environments = []
     end
   end
 end
