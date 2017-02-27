979136ebb685b2e86e73bf21a488073522cb051f
Marvin Frederickson <marvin.frederickson@gmail.com>
2017-02-27 13:20:22 -0900

    rubocop placations

--- lib/strict_priority_shuffle.rb
+++ lib/strict_priority_shuffle.rb
@@ -1,19 +1,16 @@
 require 'base_shuffle'
 
 # Weight and shuffle the content.
-# Uses a weighting and shuffling approach similiar to Concerto 1,
-# where content is added N times for the weight of each subscription
-# then jumbled up and served from a timeline.
+# includes only content belonging to the highest weighted subscription
 class StrictPriorityShuffle < BaseShuffle
-
-  def next_contents()
+  def next_contents
     prioritised_content.to_a.compact
   end
 
   private
 
   def prioritised_content
-    highest_prio=@subscriptions.select{|s| s.contents.length>0}.max_by{|s| s.weight}.weight
-    @subscriptions.select{|s| s.weight==highest_prio}.collect{|s| s.contents}.flatten.shuffle
+    highest_priority = @subscriptions.select { |s| s.contents.present? }.max_by(&:weight).weight
+    @subscriptions.select { |s| s.weight == highest_priority }.collect(&:contents).flatten.shuffle
   end
 end
