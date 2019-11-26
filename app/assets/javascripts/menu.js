be82b67f2c0d7844ee9b9a83fdf32ca79d4a0a2b
Adam Coquery <acoquery@bouyguestelecom.fr>
2019-11-26 12:33:02 +0100

    Update Menu.js. Remove font. Need to correct phone view

--- app/assets/javascripts/menu.js
+++ app/assets/javascripts/menu.js
@@ -1,8 +1,34 @@
-function menuCollapse() {
-  var nav = $("nav");
-  if (nav.hasClass("minimized")) {
-    nav.removeClass("minimized");
-  } else {
-    nav.addClass("minimized");
+
+function switch_nav(){
+  if (typeof(localStorage) !== "undefined") {
+    if(localStorage.getItem("nav")) {
+      localStorage.removeItem("nav");
+    } else {
+      localStorage.setItem("nav", 'true');
+    }
   }
+}
+
+function update(animate) {
+  var nav = $("nav");
+  if (typeof(localStorage) !== "undefined") {
+    if(localStorage.getItem("nav")) {
+    nav.addClass("minimized");
+    nav.removeClass("no-animation");
+    if(!animate) {
+      nav.addClass("no-animation");
+    }
+  } else {
+    nav.removeClass("minimized");
+    nav.removeClass("no-animation");
+    if(!animate) {
+      nav.addClass("no-animation");
+    }
+  }
+}
+}
+
+function menuCollapse() {
+  switch_nav();
+  update();
 }
