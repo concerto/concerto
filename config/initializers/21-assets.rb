fa8e761907f46bfdcf0051d7f83b187bea3a4f67
Brian Michalski <bmichalski@gmail.com>
2023-11-13 23:42:04 -0500

    Manually precompile out application.
    Again, I don't know why.

--- config/initializers/21-assets.rb
+++ config/initializers/21-assets.rb
@@ -8,9 +8,6 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder
 # are already added.
-Rails.application.config.assets.precompile += %w( html5_shiv/html5.js )
-# The HTML 5 Shiv is kep outside of application.js because it is
-# loaded conditionally for IE.
-
+Rails.application.config.assets.precompile += %w( application.css application.js )
 
 Rails.logger.debug "Completed #{File.basename(__FILE__)} at #{Time.now.to_s}"
