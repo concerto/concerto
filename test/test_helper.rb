3d28a05573e98567c8523185274ff1d5526dca84
Brian Michalski <bmichalski@gmail.com>
2023-11-16 00:09:02 -0500

    Remove unneeded patch.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,21 +5,6 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 load "#{Rails.root}/config/initializers/02-concerto_config.rb"
 
-if RUBY_VERSION>='2.6.0'
-  if Rails.version < '5'
-    class ActionController::TestResponse < ActionDispatch::TestResponse
-      def recycle!
-        # hack to avoid MonitorMixin double-initialize error:
-        @mon_mutex_owner_object_id = nil
-        @mon_mutex = nil
-        initialize
-      end
-    end
-  else
-    puts "Monkeypatch for ActionController::TestResponse no longer needed"
-  end
-end
-
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
   #
