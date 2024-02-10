0da082eb39b95989784ff1a47c0693c51b96d12e
Brian Michalski <bmichalski@gmail.com>
2024-02-10 05:56:24 +0000

    Ensure ContentType exists before using it.
    I don't understand why I didn't experience this locally.

--- app/models/graphic.rb
+++ app/models/graphic.rb
@@ -1,3 +1,5 @@
+require_relative "content"
+
 class Graphic < ApplicationRecord
   include ContentType
 
--- app/models/text_blob.rb
+++ app/models/text_blob.rb
@@ -1,3 +1,5 @@
+require_relative "content"
+
 class TextBlob < ApplicationRecord
     include ContentType
 
