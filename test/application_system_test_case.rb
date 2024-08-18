1b095faa31bc516ff8aa3e35e45eae5b4e92b942
Brian Michalski <bmichalski@gmail.com>
2024-08-18 09:19:16 +0200

    Fix indentation.

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -12,10 +12,9 @@ class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
     driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 1400 ]
   end
 
-
   # Clean out uploaded files.
-   def after_teardown
+  def after_teardown
     super
     FileUtils.rm_rf(ActiveStorage::Blob.service.root)
-   end
+  end
 end
