eac9c3591739cfd93a8d3836bfbb96a43dbe7eb5
Brian Michalski <bmichalski@gmail.com>
2024-09-06 00:36:09 -0400

    Start to render fields.

---
+++ app/frontend/components/Field.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { onMounted } from 'vue'
+
+const props = defineProps({
+  apiUrl: String
+});
+
+
+// lifecycle hooks
+onMounted(() => {
+
+})
+</script>
+
+<template>
+  <div class="field">I fetch content from {{props.apiUrl}}</div>
+</template>
+
+<style scoped>
+  .field {
+    position: absolute;
+    border: 1px dashed yellow;
+  }
+</style>
--- app/frontend/components/Screen.vue
+++ app/frontend/components/Screen.vue
@@ -1,11 +1,13 @@
 <script setup>
 import { ref, onMounted, computed } from 'vue'
+import Field from './Field.vue'
 
 const props = defineProps({
   apiUrl: String
 });
 
 const backgroundImage = ref("");
+const positions = ref([]);
 
 const backgroundImageStyle = computed(() => {
   return `url(${backgroundImage.value})`;
@@ -15,6 +17,10 @@ async function loadConfig() {
   const resp = await fetch(props.apiUrl);
   const screen = await resp.json();
   backgroundImage.value = screen.template.background_uri;
+
+  screen.positions.forEach(position => {
+    positions.value.push(position);
+  });
 }
 
 // lifecycle hooks
@@ -25,6 +31,14 @@ onMounted(() => {
 
 <template>
   <div class="screen">
+    <Field v-for="position in positions"
+      :api-url="position.content_uri"
+      :style="{
+        top: 100 * position.top + '%',
+        left: 100 * position.left + '%',
+        height: 100 * (position.bottom-position.top) + '%',
+        width: 100 * (position.right-position.left) + '%',
+      }"/>
     <div id="background"></div>
   </div>
 </template>
