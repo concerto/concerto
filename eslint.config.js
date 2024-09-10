43e2d69e349e8d57538fcb1e2ca21400f3494097
Brian Michalski <bmichalski@gmail.com>
2024-09-10 03:50:40 +0000

    Also lint the frontend tests.

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,10 @@
 import pluginVue from 'eslint-plugin-vue'
+import js from '@eslint/js'
+
 export default [
   // add more generic rulesets here, such as:
-  // js.configs.recommended,
+  js.configs.recommended,
   ...pluginVue.configs['flat/recommended'],
-  // ...pluginVue.configs['flat/vue2-recommended'], // Use this if you are using Vue.js 2.x.
   {
     rules: {
       // override/add rules settings here, such as:
