b1c367529319f8b5faaba6332c5de73ba869449f
Brian Michalski <bmichalski@gmail.com>
2024-09-12 20:48:19 -0400

    Add indentation style rule.

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,17 @@
 import pluginVue from 'eslint-plugin-vue'
 import js from '@eslint/js'
+import stylistic from '@stylistic/eslint-plugin'
 
 export default [
   // add more generic rulesets here, such as:
   js.configs.recommended,
   ...pluginVue.configs['flat/recommended'],
   {
+    plugins: {
+      '@stylistic': stylistic,
+    },
     rules: {
+      'indent': ['error', 2],
       // override/add rules settings here, such as:
       // 'vue/no-unused-vars': 'error'
     }
