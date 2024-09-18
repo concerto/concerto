50235cf404c55d67eb43b18438d0834d7b0ce951
Brian Michalski <bmichalski@gmail.com>
2024-09-18 00:14:14 -0400

    First pass as RSS feeds.
    Only supports headlines, nothing triggers this either.

--- app/models/rss_feed.rb
+++ app/models/rss_feed.rb
@@ -1,3 +1,59 @@
+require "open-uri"
+
 class RssFeed < Feed
     store_accessor :config, :url
+
+    def refresh
+      new_items = new_items()
+      existing_content = content.all
+
+      expired_time = Time.now
+
+      existing_content.each.with_index do |content, index|
+        # First, update all the existing content.
+        if new_items.length > index
+          content.text = new_items[index]
+          content.name = "#{name} (#{index + 1})"
+          content.start_time = nil
+          content.end_time = nil
+        else
+          # Then, expire any un-unpdated content.
+          content.text = ""
+          content.name = "#{name} (unused)"
+          content.end_time = expired_time
+        end
+        content.save
+      end
+
+      # If there is any new items left, create new RichText content.
+      offset = (new_items.length - existing_content.length)
+      if offset > 0
+        new_items[existing_content.length..].each.with_index do |item, index|
+          content << RichText.new(
+            render_as: "html",
+            name: "#{name} (#{index + 1})",
+            text: item
+          )
+        end
+      end
+      ""
+    end
+
+    def new_items
+        doc = Nokogiri::XML(URI.open(url))
+
+        title = doc.xpath("/rss/channel/title").text
+        items = doc.xpath("//item").map do |item|
+          item.xpath("title").text
+        end
+
+        contents = []
+
+        items.each_slice(5).with_index do |slice, index|
+          text = "<h1>#{title}</h1><h2>#{slice.join("</h2><h2>")}</h2>"
+          contents.push(text)
+        end
+
+        contents
+    end
 end
--- test/models/rss_feed_test.rb
+++ test/models/rss_feed_test.rb
@@ -1,7 +1,76 @@
 require "test_helper"
+require "minitest/mock"
 
 class RssFeedTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @feed = rss_feeds(:yahoo_rssfeed)
+  end
+
+  test "parses headlines from a simple RSS feed" do
+    mock_file = File.read("test/support/basic_rss_feed.xml")
+
+    URI.stub :open, mock_file do
+      items = @feed.new_items
+      assert_equal items.length, 2
+
+      assert_equal @feed.new_items[0], [
+        "<h1>Yahoo News - Latest News  Headlines</h1>",
+        "<h2>Item 1 Title</h2>",
+        "<h2>Item 2 Title</h2>",
+        "<h2>Item 3 Title</h2>",
+        "<h2>Item 4 Title</h2>",
+        "<h2>Item 5 Title</h2>" ].join()
+
+        assert_equal @feed.new_items[1], [
+          "<h1>Yahoo News - Latest News  Headlines</h1>",
+          "<h2>Item 6 Title</h2>",
+          "<h2>Item 7 Title</h2>",
+          "<h2>Item 8 Title</h2>",
+          "<h2>Item 9 Title</h2>",
+          "<h2>Item 10 Title</h2>" ].join()
+    end
+  end
+
+  test "creates new content items when there are none" do
+    feed = RssFeed.create()
+
+    assert_equal feed.content.length, 0
+
+    mock_items = [ "Item 1", "Item 2" ]
+    feed.stub :new_items, mock_items do
+      feed.refresh
+    end
+
+    assert_equal feed.content.length, 2
+
+    assert_equal feed.content[0].text, mock_items[0]
+    assert_nil feed.content[0].end_time
+
+    assert_equal feed.content[1].text, mock_items[1]
+    assert_nil feed.content[1].end_time
+  end
+
+  test "updates existing and expires unneeded content items" do
+    feed = RssFeed.create()
+
+    original_items = [ "Item 1", "Item 2" ]
+    feed.stub :new_items, original_items do
+      feed.refresh
+    end
+
+    # At this point, the feed should have 2 active pieces of content
+    mock_items = [ "Item 3" ]
+    feed.stub :new_items, mock_items do
+      feed.refresh
+    end
+
+    content = feed.content.sort_by { |c| c.name }
+
+    assert_equal content[0].text, mock_items[0]
+    assert_nil content[0].end_time
+
+    assert content[1].name.include?("unused")
+    assert_empty content[1].text
+    assert_operator content[1].end_time, :<, Time.now
+  end
 end
