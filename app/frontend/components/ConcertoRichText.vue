a72f097206af96c56538f6b23a3a8c6460a62fca
Brian Michalski <bmichalski@gmail.com>
2024-09-23 22:57:08 -0400

    Scale down the fontSize to make rich text fit.

--- app/frontend/components/ConcertoRichText.vue
+++ app/frontend/components/ConcertoRichText.vue
@@ -1,13 +1,38 @@
 <script setup>
+import { onMounted, ref } from 'vue'
 
 const props = defineProps({
   content: {type: Object, required: true}
 });
 
+// The main div which holds the text.
+const el = ref();
+
+async function resizeText() {
+  let element = el.value;
+
+  let displayHeight = element.scrollHeight;
+  let fieldHeight = element.offsetHeight;
+  let fontScale = 100;
+
+  // This is a very ineffecient, but simple approach.
+  while (displayHeight > fieldHeight && fontScale > 1) {
+    el.value.style.fontSize = `${fontScale}%`;
+    fontScale--;
+
+    displayHeight = element.scrollHeight;
+  }
+}
+
+// lifecycle hooks
+onMounted(() => {
+  resizeText();
+})
+
 </script>
 
 <template>
-  <div class="richtext">
+  <div ref="el" class="richtext">
     <div
       v-if="props.content.render_as == 'plaintext'"
       class="plaintext"
@@ -29,5 +54,6 @@ const props = defineProps({
     height: 100%;
     width: 100%;
     overflow: hidden;
+    font-size: 100%; /* Initial font size. */
   }
 </style>
