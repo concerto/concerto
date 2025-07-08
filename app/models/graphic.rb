76b9607f287da706e572a73b5f83dc8af4666a1e
Brian Michalski <bmichalski@gmail.com>
2025-07-07 23:53:11 -0400

    Analyze images if we skipped that somehow.

--- app/models/graphic.rb
+++ app/models/graphic.rb
@@ -20,6 +20,7 @@ class Graphic < Content
   def should_render_in?(position)
     if !image.analyzed?
       logger.debug "graphic #{id} not analyzed, fallback rendering"
+      image.analyze_later
       return super
     end
 
