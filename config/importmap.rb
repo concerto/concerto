0013dbf12c292ebcac5b59206ec7fb216a7d0e9a
Brian Michalski <bmichalski@gmail.com>
2025-12-03 23:48:14 -0500

    Update importmap dependencies.  Fixes #474.
    The importmap outdated command didn't work, so we scaffolded a fresh app, pinned, and copied over the vendored code.

--- config/importmap.rb
+++ config/importmap.rb
@@ -2,16 +2,15 @@
 
 pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
-pin "@hotwired/stimulus", to: "@hotwired--stimulus.js" # @3.2.2
+pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "stimulus-rails-nested-form" # @4.1.0
 pin "@stimulus-components/dropdown", to: "@stimulus-components--dropdown.js" # @3.0.0
 pin "stimulus-use" # @0.52.3
-pin "@tailwindcss/forms", to: "@tailwindcss--forms.js" # @0.5.7
+pin "@tailwindcss/forms", to: "@tailwindcss--forms.js" # @0.5.10
 pin "mini-svg-data-uri" # @1.4.4
-pin "picocolors" # @1.0.1
-pin "tailwindcss/colors", to: "tailwindcss--colors.js" # @3.4.4
-pin "tailwindcss/defaultTheme", to: "tailwindcss--defaultTheme.js" # @3.4.4
-pin "tailwindcss/plugin", to: "tailwindcss--plugin.js" # @3.4.4
+pin "tailwindcss/colors", to: "tailwindcss--colors.js" # @4.1.17
+pin "tailwindcss/defaultTheme", to: "tailwindcss--defaultTheme.js" # @4.1.17
+pin "tailwindcss/plugin", to: "tailwindcss--plugin.js" # @4.1.17
 pin "jszip" # @3.10.1
---
+++ vendor/javascript/tailwindcss--plugin.js
@@ -0,0 +1,4 @@
+// tailwindcss/plugin@4.1.17 downloaded from https://ga.jspm.io/npm:tailwindcss@4.1.17/dist/plugin.js
+
+var n={};function r(n,r){return{handler:n,config:r}}r.withOptions=function(n,r=()=>({})){function t(t){return{handler:n(t),config:r(t)}}return t.__isOptionsFunction=!0,t};var t=r;n=t;var i=n;export{i as default};
+
